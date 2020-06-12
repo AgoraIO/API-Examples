1071d43fc5a43c328746915c79bea89c7bbbcc8d
Chen-Alexander <1401429109@qq.com>
2020-06-12 18:47:56 +0800

    1:Modify package name. 2:Added example of CustomAudioRecord section.

--- Android/APIExample/app/src/main/java/io/agora/api/example/examples/basic/JoinChannelAudio.java
+++ Android/APIExample/app/src/main/java/io/agora/api/example/examples/basic/JoinChannelAudio.java
@@ -1,4 +1,4 @@
-package io.agora.api.example.examples.live_broadcasting;
+package io.agora.api.example.examples.basic;
 
 import android.content.Context;
 import android.os.Bundle;
@@ -6,68 +6,51 @@ import android.os.Handler;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.FrameLayout;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AlertDialog;
-import androidx.fragment.app.Fragment;
 
 import com.yanzhenjie.permission.AndPermission;
 import com.yanzhenjie.permission.runtime.Permission;
-
-import java.nio.charset.Charset;
 
 import io.agora.api.example.R;
 import io.agora.api.example.annotation.Example;
 import io.agora.api.example.common.BaseFragment;
 import io.agora.rtc.Constants;
-import io.agora.rtc.IMetadataObserver;
 import io.agora.rtc.IRtcEngineEventHandler;
 import io.agora.rtc.RtcEngine;
-import io.agora.rtc.video.VideoCanvas;
-import io.agora.rtc.video.VideoEncoderConfiguration;
 
-import static io.agora.rtc.video.VideoCanvas.RENDER_MODE_HIDDEN;
-import static io.agora.rtc.video.VideoEncoderConfiguration.FRAME_RATE.FRAME_RATE_FPS_15;
-import static io.agora.rtc.video.VideoEncoderConfiguration.ORIENTATION_MODE.ORIENTATION_MODE_ADAPTIVE;
-import static io.agora.rtc.video.VideoEncoderConfiguration.STANDARD_BITRATE;
-import static io.agora.rtc.video.VideoEncoderConfiguration.VD_640x360;
-
+/**This demo demonstrates how to make a one-to-one voice call*/
 @Example(
-        group = "Live BROADCASTING",
-        name = "Video Metadata",
-        actionId = R.id.action_mainFragment_to_VideoMetadata
+        group = "BASIC",
+        name = "JoinChannelAudio",
+        actionId = R.id.action_mainFragment_to_joinChannelAudio
 )
-public class VideoMetadata extends BaseFragment implements View.OnClickListener
+public class JoinChannelAudio extends BaseFragment implements View.OnClickListener
 {
-    public static final String TAG = VideoMetadata.class.getSimpleName();
-    private FrameLayout fl_local, fl_remote;
-    private Button send, join;
+    private static final String TAG = JoinChannelAudio.class.getSimpleName();
     private EditText et_channel;
+    private Button mute, join, speaker;
     private RtcEngine engine;
     private int myUid;
     private boolean joined = false;
-    /**
-     * Maximum length of meta data
-     */
-    private int MAX_META_SIZE = 1024;
-    /**
-     * Meta data to be sent
-     */
-    private byte[] metadata;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        handler = new Handler();
+    }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
     {
-        View view = inflater.inflate(R.layout.fragment_video_metadata, container, false);
+        View view = inflater.inflate(R.layout.fragment_joinchannel_audio, container, false);
         return view;
     }
 
@@ -75,14 +58,13 @@ public class VideoMetadata extends BaseFragment implements View.OnClickListener
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState)
     {
         super.onViewCreated(view, savedInstanceState);
-        send = view.findViewById(R.id.btn_send);
-        send.setOnClickListener(this);
-        send.setEnabled(false);
         join = view.findViewById(R.id.btn_join);
         et_channel = view.findViewById(R.id.et_channel);
         view.findViewById(R.id.btn_join).setOnClickListener(this);
-        fl_local = view.findViewById(R.id.fl_local);
-        fl_remote = view.findViewById(R.id.fl_remote);
+        mute = view.findViewById(R.id.btn_mute);
+        mute.setOnClickListener(this);
+        speaker = view.findViewById(R.id.btn_speaker);
+        speaker.setOnClickListener(this);
     }
 
     @Override
@@ -103,7 +85,7 @@ public class VideoMetadata extends BaseFragment implements View.OnClickListener
              *              How to get the App ID</a>
              * @param handler IRtcEngineEventHandler is an abstract class providing default implementation.
              *                The SDK uses this class to report to the app on SDK runtime events.*/
-            engine = RtcEngine.create(context.getApplicationContext(), getString(R.string.agora_app_id), iRtcEngineEventHandler);
+            engine = RtcEngine.create(getContext().getApplicationContext(), getString(R.string.agora_app_id), iRtcEngineEventHandler);
         }
         catch (Exception e)
         {
@@ -117,7 +99,7 @@ public class VideoMetadata extends BaseFragment implements View.OnClickListener
     {
         super.onDestroy();
         /**leaveChannel and Destroy the RtcEngine instance*/
-        if (engine != null)
+        if(engine != null)
         {
             engine.leaveChannel();
         }
@@ -143,8 +125,7 @@ public class VideoMetadata extends BaseFragment implements View.OnClickListener
                 // Request permission
                 AndPermission.with(this).runtime().permission(
                         Permission.Group.STORAGE,
-                        Permission.Group.MICROPHONE,
-                        Permission.Group.CAMERA
+                        Permission.Group.MICROPHONE
                 ).onGranted(permissions ->
                 {
                     // Permissions Granted
@@ -172,39 +153,34 @@ public class VideoMetadata extends BaseFragment implements View.OnClickListener
                  *      2:If you call the leaveChannel method during CDN live streaming, the SDK
                  *          triggers the removeInjectStreamUrl method.*/
                 engine.leaveChannel();
-                send.setEnabled(false);
                 join.setText(getString(R.string.join));
+                speaker.setText(getString(R.string.speaker));
+                speaker.setEnabled(false);
+                mute.setText(getString(R.string.closemicrophone));
+                mute.setEnabled(false);
             }
         }
-        else if (v.getId() == R.id.btn_send)
+        else if (v.getId() == R.id.btn_mute)
         {
-            /**Click once, the metadata is sent once.
-             * {@link VideoMetadata#iMetadataObserver}.
-             * The metadata here can be flexibly replaced according to your own business.*/
-            metadata = String.valueOf(System.currentTimeMillis()).getBytes(Charset.forName("UTF-8"));
+            mute.setActivated(!mute.isActivated());
+            mute.setText(getString(mute.isActivated() ? R.string.openmicrophone : R.string.closemicrophone));
+            /**Turn off / on the microphone, stop / start local audio collection and push streaming.*/
+            engine.muteLocalAudioStream(mute.isActivated());
+        }
+        else if (v.getId() == R.id.btn_speaker)
+        {
+            speaker.setActivated(!speaker.isActivated());
+            speaker.setText(getString(speaker.isActivated() ? R.string.earpiece : R.string.speaker));
+            /**Turn off / on the speaker and change the audio playback route.*/
+            engine.setEnableSpeakerphone(speaker.isActivated());
         }
     }
 
+    /**
+     * @param channelId Specify the channel name that you want to join.
+     *                  Users that input the same channel name join the same channel.*/
     private void joinChannel(String channelId)
     {
-        // Check if the context is valid
-        Context context = getContext();
-        if (context == null)
-        {
-            return;
-        }
-
-        // Create render view by RtcEngine
-        SurfaceView surfaceView = RtcEngine.CreateRendererView(context);
-        // Local video is on the top
-        surfaceView.setZOrderMediaOverlay(true);
-        // Add to the local container
-        fl_local.addView(surfaceView, new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
-        // Setup local video to render your local camera preview
-        engine.setupLocalVideo(new VideoCanvas(surfaceView, RENDER_MODE_HIDDEN, 0));
-        // Set audio route to speaker
-        engine.setDefaultAudioRoutetoSpeakerphone(true);
-
         /** Sets the channel profile of the Agora RtcEngine.
          CHANNEL_PROFILE_COMMUNICATION(0): (Default) The Communication profile.
          Use this profile in one-on-one calls or group calls, where all users can talk freely.
@@ -214,21 +190,6 @@ public class VideoMetadata extends BaseFragment implements View.OnClickListener
         engine.setChannelProfile(Constants.CHANNEL_PROFILE_LIVE_BROADCASTING);
         /**In the demo, the default is to enter as the anchor.*/
         engine.setClientRole(IRtcEngineEventHandler.ClientRole.CLIENT_ROLE_BROADCASTER);
-        // Enable video module
-        engine.enableVideo();
-        // Setup video encoding configs
-        engine.setVideoEncoderConfiguration(new VideoEncoderConfiguration(
-                VD_640x360,
-                FRAME_RATE_FPS_15,
-                STANDARD_BITRATE,
-                ORIENTATION_MODE_ADAPTIVE
-        ));
-        /**register metadata observer
-         * @return 0：方法调用成功
-         *         < 0：方法调用失败*/
-        int code = engine.registerMediaMetadataObserver(iMetadataObserver, IMetadataObserver.VIDEO_METADATA);
-        Log.e(TAG, code + "");
-
         /**Please configure accessToken in the string_config file.
          * A temporary token generated in Console. A temporary token is valid for 24 hours. For details, see
          *      https://docs.agora.io/en/Agora%20Platform/token?platform=All%20Platforms#get-a-temporary-token
@@ -256,59 +217,8 @@ public class VideoMetadata extends BaseFragment implements View.OnClickListener
         join.setEnabled(false);
     }
 
-    /**
-     * By implementing this interface, metadata can be sent and received with video frames.
-     */
-    private final IMetadataObserver iMetadataObserver = new IMetadataObserver()
-    {
-        /**Returns the maximum data size of Metadata*/
-        @Override
-        public int getMaxMetadataSize()
-        {
-            return MAX_META_SIZE;
-        }
-
-        /**Occurs when the SDK is ready to receive and send metadata.
-         * You need to specify the metadata in the return value of this callback.
-         * @param timeStampMs The timestamp (ms) of the current metadata.
-         * @return The metadata that you want to send in the format of byte[]. Ensure that you set the return value.
-         * PS: Ensure that the size of the metadata does not exceed the value set in the getMaxMetadataSize callback.*/
-        @Override
-        public byte[] onReadyToSendMetadata(long timeStampMs)
-        {
-            /**Check if the metadata is empty.*/
-            if (metadata == null)
-            {
-                return null;
-            }
-            Log.i(TAG, "There is metadata to send!");
-            /**Recycle metadata objects.*/
-            byte[] toBeSend = metadata;
-            metadata = null;
-            if (toBeSend.length > MAX_META_SIZE)
-            {
-                Log.e(TAG, String.format("Metadata exceeding max length %d!", MAX_META_SIZE));
-                return null;
-            }
-            Log.i(TAG, String.format("Metadata sent successfully! The content is %s", new String(toBeSend, Charset.forName("UTF-8"))));
-            return toBeSend;
-        }
-
-        /**Occurs when the local user receives the metadata.
-         * @param buffer The received metadata.
-         * @param uid The ID of the user who sent the metadata.
-         * @param timeStampMs The timestamp (ms) of the received metadata.*/
-        @Override
-        public void onMetadataReceived(byte[] buffer, int uid, long timeStampMs)
-        {
-            Log.i(TAG, "onMetadataReceived:" + new String(buffer, Charset.forName("UTF-8")));
-        }
-    };
-
-    /**
-     * IRtcEngineEventHandler is an abstract class providing default implementation.
-     * The SDK uses this class to report to the app on SDK runtime events.
-     */
+    /**IRtcEngineEventHandler is an abstract class providing default implementation.
+     * The SDK uses this class to report to the app on SDK runtime events.*/
     private final IRtcEngineEventHandler iRtcEngineEventHandler = new IRtcEngineEventHandler()
     {
         /**Reports a warning during SDK runtime.
@@ -357,7 +267,8 @@ public class VideoMetadata extends BaseFragment implements View.OnClickListener
                 @Override
                 public void run()
                 {
-                    send.setEnabled(true);
+                    speaker.setEnabled(true);
+                    mute.setEnabled(true);
                     join.setEnabled(true);
                     join.setText(getString(R.string.leave));
                 }
@@ -394,57 +305,13 @@ public class VideoMetadata extends BaseFragment implements View.OnClickListener
          *   REMOTE_AUDIO_REASON_REMOTE_UNMUTED(6): The remote user resumes sending the audio stream
          *              or enables the audio module.
          *   REMOTE_AUDIO_REASON_REMOTE_OFFLINE(7): The remote user leaves the channel.
-         * @param elapsed Time elapsed (ms) from the local user calling the joinChannel method
+         *   @param elapsed Time elapsed (ms) from the local user calling the joinChannel method
          *                  until the SDK triggers this callback.*/
         @Override
         public void onRemoteAudioStateChanged(int uid, int state, int reason, int elapsed)
         {
             super.onRemoteAudioStateChanged(uid, state, reason, elapsed);
             Log.i(TAG, "onRemoteAudioStateChanged->" + uid + ", state->" + state + ", reason->" + reason);
-        }
-
-        /**Since v2.9.0.
-         * Occurs when the remote video state changes.
-         * PS: This callback does not work properly when the number of users (in the Communication
-         *     profile) or broadcasters (in the Live-broadcast profile) in the channel exceeds 17.
-         * @param uid ID of the remote user whose video state changes.
-         * @param state State of the remote video:
-         *   REMOTE_VIDEO_STATE_STOPPED(0): The remote video is in the default state, probably due
-         *              to REMOTE_VIDEO_STATE_REASON_LOCAL_MUTED(3), REMOTE_VIDEO_STATE_REASON_REMOTE_MUTED(5),
-         *              or REMOTE_VIDEO_STATE_REASON_REMOTE_OFFLINE(7).
-         *   REMOTE_VIDEO_STATE_STARTING(1): The first remote video packet is received.
-         *   REMOTE_VIDEO_STATE_DECODING(2): The remote video stream is decoded and plays normally,
-         *              probably due to REMOTE_VIDEO_STATE_REASON_NETWORK_RECOVERY (2),
-         *              REMOTE_VIDEO_STATE_REASON_LOCAL_UNMUTED(4), REMOTE_VIDEO_STATE_REASON_REMOTE_UNMUTED(6),
-         *              or REMOTE_VIDEO_STATE_REASON_AUDIO_FALLBACK_RECOVERY(9).
-         *   REMOTE_VIDEO_STATE_FROZEN(3): The remote video is frozen, probably due to
-         *              REMOTE_VIDEO_STATE_REASON_NETWORK_CONGESTION(1) or REMOTE_VIDEO_STATE_REASON_AUDIO_FALLBACK(8).
-         *   REMOTE_VIDEO_STATE_FAILED(4): The remote video fails to start, probably due to
-         *              REMOTE_VIDEO_STATE_REASON_INTERNAL(0).
-         * @param reason The reason of the remote video state change:
-         *   REMOTE_VIDEO_STATE_REASON_INTERNAL(0): Internal reasons.
-         *   REMOTE_VIDEO_STATE_REASON_NETWORK_CONGESTION(1): Network congestion.
-         *   REMOTE_VIDEO_STATE_REASON_NETWORK_RECOVERY(2): Network recovery.
-         *   REMOTE_VIDEO_STATE_REASON_LOCAL_MUTED(3): The local user stops receiving the remote
-         *               video stream or disables the video module.
-         *   REMOTE_VIDEO_STATE_REASON_LOCAL_UNMUTED(4): The local user resumes receiving the remote
-         *               video stream or enables the video module.
-         *   REMOTE_VIDEO_STATE_REASON_REMOTE_MUTED(5): The remote user stops sending the video
-         *               stream or disables the video module.
-         *   REMOTE_VIDEO_STATE_REASON_REMOTE_UNMUTED(6): The remote user resumes sending the video
-         *               stream or enables the video module.
-         *   REMOTE_VIDEO_STATE_REASON_REMOTE_OFFLINE(7): The remote user leaves the channel.
-         *   REMOTE_VIDEO_STATE_REASON_AUDIO_FALLBACK(8): The remote media stream falls back to the
-         *               audio-only stream due to poor network conditions.
-         *   REMOTE_VIDEO_STATE_REASON_AUDIO_FALLBACK_RECOVERY(9): The remote media stream switches
-         *               back to the video stream after the network conditions improve.
-         * @param elapsed Time elapsed (ms) from the local user calling the joinChannel method until
-         *               the SDK triggers this callback.*/
-        @Override
-        public void onRemoteVideoStateChanged(int uid, int state, int reason, int elapsed)
-        {
-            super.onRemoteVideoStateChanged(uid, state, reason, elapsed);
-            Log.i(TAG, "onRemoteVideoStateChanged->" + uid + ", state->" + state + ", reason->" + reason);
         }
 
         /**Occurs when a remote user (Communication)/host (Live Broadcast) joins the channel.
@@ -457,32 +324,6 @@ public class VideoMetadata extends BaseFragment implements View.OnClickListener
             super.onUserJoined(uid, elapsed);
             Log.i(TAG, "onUserJoined->" + uid);
             showLongToast(String.format("user %d joined!", uid));
-            /**Check if the context is correct*/
-            Context context = getContext();
-            if (context == null) return;
-            handler.post(() ->
-            {
-                /**Display remote video stream*/
-                SurfaceView surfaceView = null;
-                if (fl_remote.getChildCount() == 0)
-                {
-                    // Create render view by RtcEngine
-                    surfaceView = RtcEngine.CreateRendererView(context);
-                    // Add to the remote container
-                    fl_remote.addView(surfaceView, new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
-                }
-                else
-                {
-                    View view = fl_remote.getChildAt(0);
-                    if (view instanceof SurfaceView)
-                    {
-                        surfaceView = (SurfaceView) view;
-                    }
-                }
-
-                // Setup remote video to render
-                engine.setupRemoteVideo(new VideoCanvas(surfaceView, RENDER_MODE_HIDDEN, uid));
-            });
         }
 
         /**Occurs when a remote user (Communication)/host (Live Broadcast) leaves the channel.
@@ -500,17 +341,6 @@ public class VideoMetadata extends BaseFragment implements View.OnClickListener
         {
             Log.i(TAG, String.format("user %d offline! reason:%d", uid, reason));
             showLongToast(String.format("user %d offline! reason:%d", uid, reason));
-            handler.post(new Runnable()
-            {
-                @Override
-                public void run()
-                {
-                    /**Clear render view
-                     Note: The video will stay at its last frame, to completely remove it you will need to
-                     remove the SurfaceView from its parent*/
-                    engine.setupRemoteVideo(new VideoCanvas(null, RENDER_MODE_HIDDEN, uid));
-                }
-            });
         }
     };
 }
