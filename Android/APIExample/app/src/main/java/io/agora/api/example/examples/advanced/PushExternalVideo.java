8da9443baf941cbdd5e0d652003ec47ed1e4fb75
Chen-Alexander <1401429109@qq.com>
2020-06-19 18:18:46 +0800

    Add PushExternalVideo example.

--- Android/APIExample/app/src/main/java/io/agora/api/example/examples/advanced/PushExternalVideo.java
+++ Android/APIExample/app/src/main/java/io/agora/api/example/examples/advanced/PushExternalVideo.java
@@ -1,11 +1,18 @@
 package io.agora.api.example.examples.advanced;
 
 import android.content.Context;
+import android.graphics.SurfaceTexture;
+import android.hardware.Camera;
+import android.opengl.EGLSurface;
+import android.opengl.GLES11Ext;
+import android.opengl.GLES20;
+import android.opengl.Matrix;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.SurfaceView;
+import android.view.TextureView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -18,18 +25,26 @@ import androidx.annotation.Nullable;
 import com.yanzhenjie.permission.AndPermission;
 import com.yanzhenjie.permission.runtime.Permission;
 
+import java.io.IOException;
+
 import io.agora.api.example.R;
 import io.agora.api.example.annotation.Example;
 import io.agora.api.example.common.BaseFragment;
+import io.agora.push_externalvideo.ProgramTextureOES;
+import io.agora.push_externalvideo.core.EglCore;
+import io.agora.push_externalvideo.core.GlUtil;
 import io.agora.rtc.Constants;
 import io.agora.rtc.IRtcEngineEventHandler;
 import io.agora.rtc.RtcEngine;
+import io.agora.rtc.gl.VideoFrame;
+import io.agora.rtc.video.AgoraVideoFrame;
 import io.agora.rtc.video.VideoCanvas;
 import io.agora.rtc.video.VideoEncoderConfiguration;
 
 import static io.agora.rtc.video.VideoCanvas.RENDER_MODE_HIDDEN;
 import static io.agora.rtc.video.VideoEncoderConfiguration.FRAME_RATE.FRAME_RATE_FPS_15;
 import static io.agora.rtc.video.VideoEncoderConfiguration.ORIENTATION_MODE.ORIENTATION_MODE_ADAPTIVE;
+import static io.agora.rtc.video.VideoEncoderConfiguration.ORIENTATION_MODE.ORIENTATION_MODE_FIXED_PORTRAIT;
 import static io.agora.rtc.video.VideoEncoderConfiguration.STANDARD_BITRATE;
 import static io.agora.rtc.video.VideoEncoderConfiguration.VD_640x360;
 
@@ -38,16 +53,35 @@ import static io.agora.rtc.video.VideoEncoderConfiguration.VD_640x360;
         name = "Push ExternalVideo",
         actionId = R.id.action_mainFragment_to_PushExternalVideo
 )
-public class PushExternalVideo extends BaseFragment implements View.OnClickListener
+public class PushExternalVideo extends BaseFragment implements View.OnClickListener, TextureView.SurfaceTextureListener,
+        SurfaceTexture.OnFrameAvailableListener
 {
     private static final String TAG = PushExternalVideo.class.getSimpleName();
+    private final int DEFAULT_CAPTURE_WIDTH = 640;
+    private final int DEFAULT_CAPTURE_HEIGHT = 480;
 
     private FrameLayout fl_local, fl_remote;
     private Button join;
     private EditText et_channel;
     private RtcEngine engine;
     private int myUid;
-    private boolean joined = false;
+    private volatile boolean joined = false;
+
+    private int mPreviewTexture;
+    private SurfaceTexture mPreviewSurfaceTexture;
+    private EglCore mEglCore;
+    private EGLSurface mDummySurface;
+    private EGLSurface mDrawSurface;
+    private ProgramTextureOES mProgram;
+    private float[] mTransform = new float[16];
+    private float[] mMVPMatrix = new float[16];
+    private boolean mMVPMatrixInit = false;
+    private Camera mCamera;
+    private int mFacing = Camera.CameraInfo.CAMERA_FACING_FRONT;
+    private boolean mPreviewing = false;
+    private int mSurfaceWidth;
+    private int mSurfaceHeight;
+    private boolean mTextureDestroyed;
 
     @Nullable
     @Override
@@ -95,17 +129,36 @@ public class PushExternalVideo extends BaseFragment implements View.OnClickListe
         }
     }
 
+
+
     @Override
     public void onDestroy()
     {
-        super.onDestroy();
         /**leaveChannel and Destroy the RtcEngine instance*/
         if(engine != null)
         {
+            /**After joining a channel, the user must call the leaveChannel method to end the
+             * call before joining another channel. This method returns 0 if the user leaves the
+             * channel and releases all resources related to the call. This method call is
+             * asynchronous, and the user has not exited the channel when the method call returns.
+             * Once the user leaves the channel, the SDK triggers the onLeaveChannel callback.
+             * A successful leaveChannel method call triggers the following callbacks:
+             *      1:The local client: onLeaveChannel.
+             *      2:The remote client: onUserOffline, if the user leaving the channel is in the
+             *          Communication channel, or is a BROADCASTER in the Live Broadcast profile.
+             * @returns 0: Success.
+             *          < 0: Failure.
+             * PS:
+             *      1:If you call the destroy method immediately after calling the leaveChannel
+             *          method, the leaveChannel process interrupts, and the SDK does not trigger
+             *          the onLeaveChannel callback.
+             *      2:If you call the leaveChannel method during CDN live streaming, the SDK
+             *          triggers the removeInjectStreamUrl method.*/
             engine.leaveChannel();
         }
         handler.post(RtcEngine::destroy);
         engine = null;
+        super.onDestroy();
     }
 
     @Override
@@ -136,32 +189,15 @@ public class PushExternalVideo extends BaseFragment implements View.OnClickListe
             }
             else
             {
-                joined = false;
-                /**After joining a channel, the user must call the leaveChannel method to end the
-                 * call before joining another channel. This method returns 0 if the user leaves the
-                 * channel and releases all resources related to the call. This method call is
-                 * asynchronous, and the user has not exited the channel when the method call returns.
-                 * Once the user leaves the channel, the SDK triggers the onLeaveChannel callback.
-                 * A successful leaveChannel method call triggers the following callbacks:
-                 *      1:The local client: onLeaveChannel.
-                 *      2:The remote client: onUserOffline, if the user leaving the channel is in the
-                 *          Communication channel, or is a BROADCASTER in the Live Broadcast profile.
-                 * @returns 0: Success.
-                 *          < 0: Failure.
-                 * PS:
-                 *      1:If you call the destroy method immediately after calling the leaveChannel
-                 *          method, the leaveChannel process interrupts, and the SDK does not trigger
-                 *          the onLeaveChannel callback.
-                 *      2:If you call the leaveChannel method during CDN live streaming, the SDK
-                 *          triggers the removeInjectStreamUrl method.*/
-                engine.leaveChannel();
-                join.setText(getString(R.string.join));
+                fl_local.setVisibility(View.GONE);
+                getActivity().onBackPressed();
             }
         }
     }
 
     private void joinChannel(String channelId)
     {
+//        engine.setParameters("{\"rtc.log_filter\":65535}");
         // Check if the context is valid
         Context context = getContext();
         if (context == null)
@@ -170,13 +206,12 @@ public class PushExternalVideo extends BaseFragment implements View.OnClickListe
         }
 
         // Create render view by RtcEngine
-        SurfaceView surfaceView = RtcEngine.CreateRendererView(context);
-        // Local video is on the top
-        surfaceView.setZOrderMediaOverlay(true);
+        TextureView textureView = new TextureView(getContext());
+        //add SurfaceTextureListener
+        textureView.setSurfaceTextureListener(this);
         // Add to the local container
-        fl_local.addView(surfaceView, new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
-        // Setup local video to render your local camera preview
-        engine.setupLocalVideo(new VideoCanvas(surfaceView, RENDER_MODE_HIDDEN, 0));
+        fl_local.addView(textureView, new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT));
         // Set audio route to speaker
         engine.setDefaultAudioRoutetoSpeakerphone(true);
 
@@ -189,15 +224,27 @@ public class PushExternalVideo extends BaseFragment implements View.OnClickListe
         engine.setChannelProfile(Constants.CHANNEL_PROFILE_LIVE_BROADCASTING);
         /**In the demo, the default is to enter as the anchor.*/
         engine.setClientRole(IRtcEngineEventHandler.ClientRole.CLIENT_ROLE_BROADCASTER);
-        // Enable video module
+        // Enables the video module.
         engine.enableVideo();
         // Setup video encoding configs
         engine.setVideoEncoderConfiguration(new VideoEncoderConfiguration(
-                VD_640x360,
+                new VideoEncoderConfiguration.VideoDimensions(DEFAULT_CAPTURE_WIDTH, DEFAULT_CAPTURE_HEIGHT),
                 FRAME_RATE_FPS_15,
                 STANDARD_BITRATE,
-                ORIENTATION_MODE_ADAPTIVE
+                ORIENTATION_MODE_FIXED_PORTRAIT
         ));
+        /**Configures the external video source.
+         * @param enable Sets whether or not to use the external video source:
+         *                 true: Use the external video source.
+         *                 false: Do not use the external video source.
+         * @param useTexture Sets whether or not to use texture as an input:
+         *                     true: Use texture as an input.
+         *                     false: (Default) Do not use texture as an input.
+         * @param pushMode Sets whether or not the external video source needs to call the PushExternalVideoFrame
+         *                 method to send the video frame to the Agora SDK:
+         *                   true: Use the push mode.
+         *                   false: Use the pull mode (not supported).*/
+        engine.setExternalVideoSource(true, true, true);
 
         /**Please configure accessToken in the string_config file.
          * A temporary token generated in Console. A temporary token is valid for 24 hours. For details, see
@@ -223,6 +270,152 @@ public class PushExternalVideo extends BaseFragment implements View.OnClickListe
         }
         // Prevent repeated entry
         join.setEnabled(false);
+    }
+
+    @Override
+    public void onFrameAvailable(SurfaceTexture surfaceTexture)
+    {
+        if (mTextureDestroyed) return;
+
+        if (!mEglCore.isCurrent(mDrawSurface))
+        {
+            mEglCore.makeCurrent(mDrawSurface);
+        }
+        try
+        {
+            mPreviewSurfaceTexture.updateTexImage();
+            mPreviewSurfaceTexture.getTransformMatrix(mTransform);
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
+        /**The rectangle ratio of frames and the screen surface may be different, so cropping may
+         *  happen when display frames to the screen.
+         * The display transformation matrix does not change for the same camera when the screen
+         *  orientation remains the same.*/
+        if(!mMVPMatrixInit)
+        {
+            /***/
+            /**For simplicity, we only consider the activity as portrait mode. In this case, the captured
+             * images should be rotated 90 degrees (left or right).Thus the frame width and height
+             * should be swapped.*/
+            float frameRatio = DEFAULT_CAPTURE_HEIGHT / (float) DEFAULT_CAPTURE_WIDTH;
+            float surfaceRatio = mSurfaceWidth / (float) mSurfaceHeight;
+            Matrix.setIdentityM(mMVPMatrix, 0);
+
+            if (frameRatio >= surfaceRatio)
+            {
+                float w = DEFAULT_CAPTURE_WIDTH * surfaceRatio;
+                float scaleW = DEFAULT_CAPTURE_HEIGHT / w;
+                Matrix.scaleM(mMVPMatrix, 0, scaleW, 1, 1);
+            }
+            else
+            {
+                float h = DEFAULT_CAPTURE_HEIGHT / surfaceRatio;
+                float scaleH = DEFAULT_CAPTURE_WIDTH / h;
+                Matrix.scaleM(mMVPMatrix, 0, 1, scaleH, 1);
+            }
+            mMVPMatrixInit = true;
+        }
+        GLES20.glViewport(0, 0, mSurfaceWidth, mSurfaceHeight);
+        mProgram.drawFrame(mPreviewTexture, mTransform, mMVPMatrix);
+        mEglCore.swapBuffers(mDrawSurface);
+
+        if (joined)
+        {
+            /**about AgoraVideoFrame, see https://docs.agora.io/en/Video/API%20Reference/java/classio_1_1agora_1_1rtc_1_1video_1_1_agora_video_frame.html*/
+            AgoraVideoFrame frame = new AgoraVideoFrame();
+            frame.textureID = mPreviewTexture;
+            frame.format = AgoraVideoFrame.FORMAT_TEXTURE_OES;
+            frame.transform = mTransform;
+            frame.stride = DEFAULT_CAPTURE_HEIGHT;
+            frame.height = DEFAULT_CAPTURE_WIDTH;
+            frame.eglContext14 = mEglCore.getEGLContext();
+            frame.timeStamp = System.currentTimeMillis();
+            /**Pushes the video frame using the AgoraVideoFrame class and passes the video frame to the Agora SDK.
+             * Call the setExternalVideoSource method and set pushMode as true before calling this
+             * method. Otherwise, a failure returns after calling this method.
+             * @param frame AgoraVideoFrame
+             * @return
+             *   true: The frame is pushed successfully.
+             *   false: Failed to push the frame.
+             * PS:
+             *   In the Communication profile, the SDK does not support textured video frames.*/
+            boolean a = engine.pushExternalVideoFrame(frame);
+            Log.e(TAG, "pushExternalVideoFrame:" + a);
+        }
+    }
+
+    @Override
+    public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height)
+    {
+        Log.i(TAG, "onSurfaceTextureAvailable");
+        mTextureDestroyed = false;
+        mSurfaceWidth = width;
+        mSurfaceHeight = height;
+        mEglCore = new EglCore();
+        mDummySurface = mEglCore.createOffscreenSurface(1, 1);
+        mEglCore.makeCurrent(mDummySurface);
+        mPreviewTexture = GlUtil.createTextureObject(GLES11Ext.GL_TEXTURE_EXTERNAL_OES);
+        mPreviewSurfaceTexture = new SurfaceTexture(mPreviewTexture);
+        mPreviewSurfaceTexture.setOnFrameAvailableListener(this);
+        mDrawSurface = mEglCore.createWindowSurface(surface);
+        mProgram = new ProgramTextureOES();
+        if (mCamera != null || mPreviewing)
+        {
+            Log.e(TAG, "Camera preview has been started");
+            return;
+        }
+        try
+        {
+            mCamera = Camera.open(mFacing);
+            /**It is assumed to capture images of resolution 640x480. During development, it should
+             * be the most suitable supported resolution that best fits the scenario.*/
+            Camera.Parameters parameters = mCamera.getParameters();
+            parameters.setPreviewSize(DEFAULT_CAPTURE_WIDTH, DEFAULT_CAPTURE_HEIGHT);
+            mCamera.setParameters(parameters);
+            mCamera.setPreviewTexture(mPreviewSurfaceTexture);
+            /**The display orientation is 90 for both front and back facing cameras using a surface
+             * texture for the preview when the screen is in portrait mode.*/
+            mCamera.setDisplayOrientation(90);
+            mCamera.startPreview();
+            mPreviewing = true;
+        }
+        catch (IOException e)
+        {e.printStackTrace();}
+    }
+
+    @Override
+    public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height)
+    {
+
+    }
+
+    @Override
+    public boolean onSurfaceTextureDestroyed(SurfaceTexture surface)
+    {
+        Log.i(TAG, "onSurfaceTextureDestroyed");
+        mTextureDestroyed = true;
+        if (mCamera != null && mPreviewing)
+        {
+            mCamera.stopPreview();
+            mPreviewing = false;
+            mCamera.release();
+            mCamera = null;
+        }
+        mProgram.release();
+        mEglCore.releaseSurface(mDummySurface);
+        mEglCore.releaseSurface(mDrawSurface);
+        mEglCore.release();
+        return true;
+    }
+
+    @Override
+    public void onSurfaceTextureUpdated(SurfaceTexture surface)
+    {
+
     }
 
     /**
@@ -382,25 +575,18 @@ public class PushExternalVideo extends BaseFragment implements View.OnClickListe
             handler.post(() ->
             {
                 /**Display remote video stream*/
-                SurfaceView surfaceView = null;
-                if (fl_remote.getChildCount() == 0)
+                // Create render view by RtcEngine
+                SurfaceView surfaceView = RtcEngine.CreateRendererView(context);
+                if (fl_remote.getChildCount() > 0)
                 {
-                    // Create render view by RtcEngine
-                    surfaceView = RtcEngine.CreateRendererView(context);
-                    // Add to the remote container
-                    fl_remote.addView(surfaceView, new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+                    fl_remote.removeAllViews();
                 }
-                else
-                {
-                    View view = fl_remote.getChildAt(0);
-                    if (view instanceof SurfaceView)
-                    {
-                        surfaceView = (SurfaceView) view;
-                    }
-                }
-
+                // Add to the remote container
+                fl_remote.addView(surfaceView, new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+                        ViewGroup.LayoutParams.MATCH_PARENT));
                 // Setup remote video to render
                 engine.setupRemoteVideo(new VideoCanvas(surfaceView, RENDER_MODE_HIDDEN, uid));
+//                engine.muteRemoteVideoStream(uid, false);
             });
         }
 
