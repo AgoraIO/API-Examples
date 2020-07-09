d7ab9d11fcf73c94f5265cbeb460264e20d40e95
CLuCheng <1551394539@qq.com>
2020-07-09 21:38:38 +0800

    install script.
    Former-commit-id: 802c0846c9aa6ef557788ff7ef3e4bb43c5a120d

---
+++ windows/APIExample/install.ps1
@@ -0,0 +1,23 @@
+$ThirdPartysrc = 'https://github.com/AgoraIO/Advanced-Video/releases/download/Dshow/ThirdParty.zip'
+$ThirdPartydes = 'ThirdParty.zip'
+$agora_sdk = 'https://download.agora.io/sdk/release/Agora_Native_SDK_for_Windows_v3_0_1_1_FULL.zip'
+$agora_des = 'Agora_Native_SDK_for_Windows_v3_0_1_1_FULL.zip'
+
+
+
+if (-not (Test-Path $ThirdPartydes)){
+	echo "download $LocalFileName"
+	mkdir ThirdParty
+	Invoke-WebRequest -uri $ThirdPartysrc -OutFile $ThirdPartydes -TimeoutSec 10;
+	Unblock-File $ThirdPartydes
+	tar -zxvf $ThirdPartydes -C ThirdParty
+}
+
+
+if (-not (Test-Path $agora_des)){
+	echo "download $LocalFileName"
+	Invoke-WebRequest -uri $agora_sdk -OutFile $agora_des -TimeoutSec 10;
+	Unblock-File $agora_des
+	tar -zxvf $agora_des -C .
+	Move-Item Agora_Native_SDK_for_Windows_FULL\libs libs
+}
