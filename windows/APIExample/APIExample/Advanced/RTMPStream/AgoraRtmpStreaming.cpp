79a6a5e8e541ba7828932bb185ef31ead9570cc5
CLuCheng <1551394539@qq.com>
2020-07-17 15:32:49 +0800

    APP_ID from file or code.
    Former-commit-id: d8ee1aabfbef98a635c40eed4ed630b737eb7c0a

--- windows/APIExample/APIExample/Advanced/RTMPStream/AgoraRtmpStreaming.cpp
+++ windows/APIExample/APIExample/Advanced/RTMPStream/AgoraRtmpStreaming.cpp
@@ -187,7 +187,7 @@ bool CAgoraRtmpStreamingDlg::InitAgora()
     m_eventHandler.SetMsgReceiver(m_hWnd);
 
     RtcEngineContext context;
-    context.appId = APP_ID;
+    context.appId = GET_APP_ID.c_str();
     context.eventHandler = &m_eventHandler;
     //initialize the Agora RTC engine context.  
     int ret = m_rtcEngine->initialize(context);
