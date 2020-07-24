2d333e84fa42339571be20defc656ca6a184bb97
QianzeZhang <zhangqianze@agora.io>
2020-07-24 18:39:49 +0800

    add voice changer

--- iOS/APIExample/Common/UITypeAlias.swift
+++ iOS/APIExample/Common/UITypeAlias.swift
@@ -12,6 +12,35 @@ import UIKit
 import Cocoa
 #endif
 
+typealias Color = UIColor
+
+extension UIColor {
+    
+    /// Get color rgba components in order.
+    func rgba() -> (r: CGFloat, g: CGFloat, b: CGFloat, a: CGFloat) {
+        let components = self.cgColor.components
+        let numberOfComponents = self.cgColor.numberOfComponents
+        
+        switch numberOfComponents {
+        case 4:
+            return (components![0], components![1], components![2], components![3])
+        case 2:
+            return (components![0], components![0], components![0], components![1])
+        default:
+            // FIXME: Fallback to black
+            return (0, 0, 0, 1)
+        }
+    }
+    
+    /// Check the black or white contrast on given color.
+    func blackOrWhiteContrastingColor() -> Color {
+        let rgbaT = rgba()
+        let value = 1 - ((0.299 * rgbaT.r) + (0.587 * rgbaT.g) + (0.114 * rgbaT.b));
+        return value < 0.5 ? Color.black : Color.white
+    }
+    
+}
+
 //MARK: - Color
 #if os(iOS)
 typealias AGColor = UIColor
