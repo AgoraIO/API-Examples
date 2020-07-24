c5166414402ca21f56c2d40193e7e144e56b2600
QianzeZhang <zhangqianze@agora.io>
2020-07-24 18:39:49 +0800

    add voice changer
    Former-commit-id: 2d333e84fa42339571be20defc656ca6a184bb97

---
+++ iOS/APIExample/Examples/Advanced/VoiceChanger.swift
@@ -0,0 +1,214 @@
+//
+//  VoiceChanger.swift
+//  APIExample
+//
+//  Created by 张乾泽 on 2020/7/24.
+//  Copyright © 2020 Agora Corp. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import AgoraRtcKit
+import PopMenu
+
+struct VoiceChangerItem{
+    var title: String
+    var value: AgoraAudioVoiceChanger
+}
+
+struct VoiceReverbItem{
+    var title: String
+    var value: AgoraAudioReverbPreset
+}
+
+class VoiceChanger: BaseViewController {
+    var agoraKit: AgoraRtcEngineKit!
+    @IBOutlet weak var voiceChanger: UIButton!
+    @IBOutlet weak var voiceBeauty: UIButton!
+    @IBOutlet weak var reverb: UIButton!
+    
+    var voiceChangeItems:[VoiceChangerItem] = [
+        VoiceChangerItem(title: "Off", value: .voiceChangerOff),
+        VoiceChangerItem(title: "Old Man", value: .voiceChangerOldMan),
+        VoiceChangerItem(title: "Baby Boy", value: .voiceChangerBabyBoy),
+        VoiceChangerItem(title: "Baby Girl", value: .voiceChangerBabyGirl),
+        VoiceChangerItem(title: "Zhu Ba Jie", value: .voiceChangerZhuBaJie),
+        VoiceChangerItem(title: "Ethereal", value: .voiceChangerEthereal),
+        VoiceChangerItem(title: "Hulk", value: .voiceChangerHulk)
+    ]
+    
+    var voiceBeautyItems:[VoiceChangerItem] = [
+        VoiceChangerItem(title: "Vigorous", value: .voiceBeautyVigorous),
+        VoiceChangerItem(title: "Deep", value: .voiceBeautyDeep),
+        VoiceChangerItem(title: "Mellow", value: .voiceBeautyMellow),
+        VoiceChangerItem(title: "Falsetto", value: .voiceBeautyFalsetto),
+        VoiceChangerItem(title: "Full", value: .voiceBeautyFull),
+        VoiceChangerItem(title: "Clear", value: .voiceBeautyClear),
+        VoiceChangerItem(title: "Resounding", value: .voiceBeautyResounding),
+        VoiceChangerItem(title: "Ringing", value: .voiceBeautyRinging),
+        VoiceChangerItem(title: "Spacial", value: .voiceBeautySpacial),
+        VoiceChangerItem(title: "Male Magnetic", value: .generalBeautyVoiceMaleMagnetic),
+        VoiceChangerItem(title: "Female Fresh", value: .generalBeautyVoiceFemaleFresh),
+        VoiceChangerItem(title: "Female Vitality", value: .generalBeautyVoiceFemaleVitality)
+    ]
+    
+    var reverbItems:[VoiceReverbItem] = [
+        VoiceReverbItem(title: "Popular", value: .popular),
+        VoiceReverbItem(title: "RNB", value: .rnB),
+        VoiceReverbItem(title: "Rock", value: .rock),
+        VoiceReverbItem(title: "HipHop", value: .hipHop),
+        VoiceReverbItem(title: "Vocal Concert", value: .vocalConcert),
+        VoiceReverbItem(title: "KTV", value: .KTV),
+        VoiceReverbItem(title: "Studio", value: .studio),
+        VoiceReverbItem(title: "fx KTV", value: .fxKTV),
+        VoiceReverbItem(title: "fx Vocal Concert", value: .fxVocalConcert),
+        VoiceReverbItem(title: "fx Uncle", value: .fxUncle),
+        VoiceReverbItem(title: "fx Sister", value: .fxSister),
+        VoiceReverbItem(title: "fx Studio", value: .fxStudio),
+        VoiceReverbItem(title: "fx Popular", value: .fxPopular),
+        VoiceReverbItem(title: "fx RNB", value: .fxRNB),
+        VoiceReverbItem(title: "fx Phonograph", value: .fxPhonograph),
+        VoiceReverbItem(title: "fx Virtual Stereo", value: .virtualStereo)
+    ]
+    
+    // indicate if current instance has joined channel
+    var isJoined: Bool = false
+    
+    /// callback when voice changer button hit
+    @IBAction func onVoiceChanger() {
+        let actions = voiceChangeItems.map { (item:VoiceChangerItem) -> PopMenuAGAction in
+            let action = PopMenuAGAction(title: item.title, didSelect: {[unowned self]select in
+                guard let action:PopMenuAGAction = select as? PopMenuAGAction, let val =
+                action.value as? AgoraAudioVoiceChanger else {return}
+                
+                let result = self.agoraKit.setLocalVoiceChanger(val)
+                LogUtils.log(message: "setLocalVoiceChanger \(val), result: \(result)", level: .info)
+                if(result < 0) {
+                    self.showAlert(message: "setLocalVoiceChanger failed: \(result)")
+                }
+            })
+            action.value = item.value as AnyObject
+            return action
+        }
+        self.getPrompt(actions: actions).present()
+    }
+    
+    /// callback when voice beauty button hit
+    @IBAction func onVoiceBeauty() {
+        let actions = voiceBeautyItems.map { (item:VoiceChangerItem) -> PopMenuAGAction in
+            let action = PopMenuAGAction(title: item.title, didSelect: {[unowned self]select in
+                guard let action:PopMenuAGAction = select as? PopMenuAGAction, let val =
+                action.value as? AgoraAudioVoiceChanger else {return}
+                
+                let result = self.agoraKit.setLocalVoiceChanger(val)
+                LogUtils.log(message: "setLocalVoiceChanger \(val), result: \(result)", level: .info)
+                if(result < 0) {
+                    self.showAlert(message: "setLocalVoiceChanger failed: \(result)")
+                }
+            })
+            action.value = item.value as AnyObject
+            return action
+        }
+        self.getPrompt(actions: actions).present()
+    }
+    
+    /// callback when reverb button hit
+    @IBAction func onReverb() {
+        let actions = reverbItems.map { (item:VoiceReverbItem) -> PopMenuAGAction in
+            let action = PopMenuAGAction(title: item.title, didSelect: {[unowned self]select in
+                guard let action:PopMenuAGAction = select as? PopMenuAGAction, let val =
+                action.value as? AgoraAudioReverbPreset else {return}
+                
+                let result = self.agoraKit.setLocalVoiceReverbPreset(val)
+                LogUtils.log(message: "setLocalVoiceReverbPreset \(val), result: \(result)", level: .info)
+                if(result < 0) {
+                    self.showAlert(message: "setLocalVoiceReverbPreset failed: \(result)")
+                }
+            })
+            action.value = item.value as AnyObject
+            return action
+        }
+        self.getPrompt(actions: actions).present()
+    }
+    
+    override func viewDidLoad(){
+        super.viewDidLoad()
+        // set up agora instance when view loaded
+        agoraKit = AgoraRtcEngineKit.sharedEngine(withAppId: KeyCenter.AppId, delegate: self)
+        
+        guard let channelName = configs["channelName"] as? String else {return}
+        
+        // disable video module
+        agoraKit.disableVideo()
+        
+        // Set audio route to speaker
+        agoraKit.setDefaultAudioRouteToSpeakerphone(true)
+        
+        // start joining channel
+        // 1. Users can only see each other after they join the
+        // same channel successfully using the same app id.
+        // 2. If app certificate is turned on at dashboard, token is needed
+        // when joining channel. The channel name and uid used to calculate
+        // the token has to match the ones used for channel join
+        let result = agoraKit.joinChannel(byToken: nil, channelId: channelName, info: nil, uid: 0) {[unowned self] (channel, uid, elapsed) -> Void in
+            self.isJoined = true
+            LogUtils.log(message: "Join \(channel) with uid \(uid) elapsed \(elapsed)ms", level: .info)
+        }
+        if result != 0 {
+            // Usually happens with invalid parameters
+            // Error code description can be found at:
+            // en: https://docs.agora.io/en/Voice/API%20Reference/oc/Constants/AgoraErrorCode.html
+            // cn: https://docs.agora.io/cn/Voice/API%20Reference/oc/Constants/AgoraErrorCode.html
+            self.showAlert(title: "Error", message: "joinChannel call failed: \(result), please check your params")
+        }
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        // leave channel when exiting the view
+        if isJoined {
+            agoraKit.leaveChannel { (stats) -> Void in
+                LogUtils.log(message: "left channel, duration: \(stats.duration)", level: .info)
+            }
+        }
+    }
+}
+
+/// agora rtc engine delegate events
+extension VoiceChanger: AgoraRtcEngineDelegate {
+    /// callback when warning occured for agora sdk, warning can usually be ignored, still it's nice to check out
+    /// what is happening
+    /// Warning code description can be found at:
+    /// en: https://docs.agora.io/en/Voice/API%20Reference/oc/Constants/AgoraWarningCode.html
+    /// cn: https://docs.agora.io/cn/Voice/API%20Reference/oc/Constants/AgoraWarningCode.html
+    /// @param warningCode warning code of the problem
+    func rtcEngine(_ engine: AgoraRtcEngineKit, didOccurWarning warningCode: AgoraWarningCode) {
+        LogUtils.log(message: "warning: \(warningCode.description)", level: .warning)
+    }
+    
+    /// callback when error occured for agora sdk, you are recommended to display the error descriptions on demand
+    /// to let user know something wrong is happening
+    /// Error code description can be found at:
+    /// en: https://docs.agora.io/en/Voice/API%20Reference/oc/Constants/AgoraErrorCode.html
+    /// cn: https://docs.agora.io/cn/Voice/API%20Reference/oc/Constants/AgoraErrorCode.html
+    /// @param errorCode error code of the problem
+    func rtcEngine(_ engine: AgoraRtcEngineKit, didOccurError errorCode: AgoraErrorCode) {
+        LogUtils.log(message: "error: \(errorCode)", level: .error)
+        self.showAlert(title: "Error", message: "Error \(errorCode.description) occur")
+    }
+    
+    /// callback when a remote user is joinning the channel, note audience in live broadcast mode will NOT trigger this event
+    /// @param uid uid of remote joined user
+    /// @param elapsed time elapse since current sdk instance join the channel in ms
+    func rtcEngine(_ engine: AgoraRtcEngineKit, didJoinedOfUid uid: UInt, elapsed: Int) {
+        LogUtils.log(message: "remote user join: \(uid) \(elapsed)ms", level: .info)
+    }
+    
+    /// callback when a remote user is leaving the channel, note audience in live broadcast mode will NOT trigger this event
+    /// @param uid uid of remote joined user
+    /// @param reason reason why this user left, note this event may be triggered when the remote user
+    /// become an audience in live broadcasting profile
+    func rtcEngine(_ engine: AgoraRtcEngineKit, didOfflineOfUid uid: UInt, reason: AgoraUserOfflineReason) {
+        LogUtils.log(message: "remote user left: \(uid) reason \(reason)", level: .info)
+    }
+}
