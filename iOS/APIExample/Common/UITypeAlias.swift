548f3f2dec739da0dcb390260f548c18100d5af0
QianzeZhang <zhangqianze@agora.io>
2020-07-27 01:58:48 +0800

    enhancements
    Former-commit-id: 8eac463058e4238ee65b06127d846f3e4bbb279b

--- iOS/APIExample/Common/UITypeAlias.swift
+++ iOS/APIExample/Common/UITypeAlias.swift
@@ -14,6 +14,30 @@ import Cocoa
 
 typealias Color = UIColor
 
+typealias MainFont = Font.HelveticaNeue
+
+enum Font {
+    enum HelveticaNeue: String {
+        case ultraLightItalic = "UltraLightItalic"
+        case medium = "Medium"
+        case mediumItalic = "MediumItalic"
+        case ultraLight = "UltraLight"
+        case italic = "Italic"
+        case light = "Light"
+        case thinItalic = "ThinItalic"
+        case lightItalic = "LightItalic"
+        case bold = "Bold"
+        case thin = "Thin"
+        case condensedBlack = "CondensedBlack"
+        case condensedBold = "CondensedBold"
+        case boldItalic = "BoldItalic"
+        
+        func with(size: CGFloat) -> UIFont {
+            return UIFont(name: "HelveticaNeue-\(rawValue)", size: size)!
+        }
+    }
+}
+
 extension UIColor {
     
     /// Get color rgba components in order.
