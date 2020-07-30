20862f92ba816730d6d01136c58491b1cfa04354
Qianze Zhang <qianze.zhang@hotmail.com>
2020-07-31 00:24:07 +0800

    Update install.ps1

--- windows/APIExample/install.ps1
+++ windows/APIExample/install.ps1
@@ -1,4 +1,4 @@
-$ThirdPartysrc = 'https://gitee.com/plutoless/API-Examples/attach_files/445294/download'
+$ThirdPartysrc = 'https://github.com/AgoraIO/Advanced-Video/releases/download/Dshow/ThirdParty.zip'
 $ThirdPartydes = 'ThirdParty.zip'
 $agora_sdk = 'https://download.agora.io/sdk/release/Agora_Native_SDK_for_Windows_v3_0_1_1_FULL.zip'
 $agora_des = 'Agora_Native_SDK_for_Windows_v3_0_1_1_FULL.zip'
