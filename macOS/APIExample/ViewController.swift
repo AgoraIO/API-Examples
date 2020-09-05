da09eb226d0f00fa45e3ca77d5bd64470876cb19
QianzeZhang <zhangqianze@agora.io>
2020-09-05 13:16:00 +0800

    add custom audio source + custom audio render

--- macOS/APIExample/ViewController.swift
+++ macOS/APIExample/ViewController.swift
@@ -23,6 +23,8 @@ class MenuController: NSViewController {
         MenuItem(name: "RTMP Streaming", identifier: "menuCell", controller: "RTMPStreaming"),
         MenuItem(name: "Custom Video Source(MediaIO)", identifier: "menuCell", controller: "CustomVideoSourceMediaIO"),
         MenuItem(name: "Custom Video Source(Push)", identifier: "menuCell", controller: "CustomVideoSourcePush"),
+        MenuItem(name: "Custom Audio Source", identifier: "menuCell", controller: "CustomAudioSource"),
+        MenuItem(name: "Custom Audio Render", identifier: "menuCell", controller: "CustomAudioRender"),
         MenuItem(name: "Raw Media Data", identifier: "menuCell", controller: "RawMediaData"),
         MenuItem(name: "Join Multiple Channels", identifier: "menuCell", controller: "JoinMultipleChannel")
     ]
