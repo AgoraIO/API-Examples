18353589f37a6354d9c463288db2c1e69fa7e2bb
xianing <xianing@agora.io>
2020-09-28 20:01:00 +0800

    Add API examples for pre call test and adjust in call volume

--- Android/APIExample/app/src/main/java/io/agora/api/example/common/model/StatisticsInfo.java
+++ Android/APIExample/app/src/main/java/io/agora/api/example/common/model/StatisticsInfo.java
@@ -8,6 +8,8 @@ public class StatisticsInfo {
     private RemoteVideoStats remoteVideoStats;
     private RemoteAudioStats remoteAudioStats;
     private RtcStats rtcStats;
+    private int quality;
+    private LastmileProbeResult lastMileProbeResult;
 
     public void setLocalVideoStats(LocalVideoStats localVideoStats) {
         this.localVideoStats = localVideoStats;
@@ -96,6 +98,71 @@ public class StatisticsInfo {
                 .append("AQuality: ")
                 .append(remoteAudioStats.quality)
                 .toString();
+    }
+
+    public void setLastMileQuality(int quality) {
+        this.quality = quality;
+    }
+
+    public String getLastMileQuality(){
+        switch (quality){
+            case 1:
+                return "EXCELLENT";
+            case 2:
+                return "GOOD";
+            case 3:
+                return "POOR";
+            case 4:
+                return "BAD";
+            case 5:
+                return "VERY BAD";
+            case 6:
+                return "DOWN";
+            case 7:
+                return "UNSUPPORTED";
+            case 8:
+                return "DETECTING";
+            default:
+                return "UNKNOWN";
+        }
+    }
+
+    public String getLastMileResult() {
+        if(lastMileProbeResult == null)
+            return null;
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("Rtt: ")
+                .append(lastMileProbeResult.rtt)
+                .append("ms")
+                .append("\n")
+                .append("DownlinkAvailableBandwidth: ")
+                .append(lastMileProbeResult.downlinkReport.availableBandwidth)
+                .append("Kbps")
+                .append("\n")
+                .append("DownlinkJitter: ")
+                .append(lastMileProbeResult.downlinkReport.jitter)
+                .append("ms")
+                .append("\n")
+                .append("DownlinkLoss: ")
+                .append(lastMileProbeResult.downlinkReport.packetLossRate)
+                .append("%")
+                .append("\n")
+                .append("UplinkAvailableBandwidth: ")
+                .append(lastMileProbeResult.uplinkReport.availableBandwidth)
+                .append("Kbps")
+                .append("\n")
+                .append("UplinkJitter: ")
+                .append(lastMileProbeResult.uplinkReport.jitter)
+                .append("ms")
+                .append("\n")
+                .append("UplinkLoss: ")
+                .append(lastMileProbeResult.uplinkReport.packetLossRate)
+                .append("%");
+        return stringBuilder.toString();
+    }
+
+    public void setLastMileProbeResult(LastmileProbeResult lastmileProbeResult) {
+        this.lastMileProbeResult = lastmileProbeResult;
     }
 
 }
