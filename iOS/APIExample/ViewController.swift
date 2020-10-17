3cc78c7f4fd74b7fa623023937d947f4865c822a
QianzeZhang <zhangqianze@agora.io>
2020-10-17 15:55:30 +0800

    Revert "remove 3.2.0 features"
    This reverts commit 61bcea0775c8f255d440a693f7f6596cfbce314a.

--- iOS/APIExample/ViewController.swift
+++ iOS/APIExample/ViewController.swift
@@ -45,6 +45,7 @@ class ViewController: AGViewController {
             MenuItem(name: "Precall Test".localized, storyboard: "PrecallTest", controller: ""),
             MenuItem(name: "Media Player".localized, storyboard: "MediaPlayer", controller: ""),
             MenuItem(name: "Screen Share".localized, storyboard: "ScreenShare", controller: ""),
+            MenuItem(name: "Super Resolution".localized, storyboard: "SuperResolution", controller: ""),
             MenuItem(name: "Media Channel Relay".localized, storyboard: "MediaChannelRelay", controller: "")
         ]),
     ]
