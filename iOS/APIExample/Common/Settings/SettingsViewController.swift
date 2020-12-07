56f339000af87a3a7c656308159bc27cef253290
Qianze Zhang <qianze.zhang@hotmail.com>
2020-12-07 20:33:28 +0800

    initial settings
    (cherry picked from commit a62d409fa3ee9e358835c815530002e8394ae37b)

--- iOS/APIExample/Common/Settings/SettingsViewController.swift
+++ iOS/APIExample/Common/Settings/SettingsViewController.swift
@@ -10,66 +10,7 @@ import Foundation
 import UIKit
 
 protocol SettingsViewControllerDelegate: AnyObject {
-    func didChangeValue(key:String, value: AnyObject)
-}
-
-class SettingsBaseCell : UITableViewCell
-{
-    var configs:SettingsBaseParam?
-    weak var delegate:SettingsViewControllerDelegate?
-    func configure(configs:SettingsBaseParam){
-        self.configs = configs
-    }
-}
-
-class SettingsBaseParam: NSObject
-{
-    var key:String
-    var label:String
-    var type:String
-    
-    init(key:String, label:String, type:String) {
-        self.key = key
-        self.label = label
-        self.type = type
-    }
-}
-
-class SettingsSliderCell : SettingsBaseCell
-{
-    @IBOutlet weak var settingLabel: AGLabel!
-    @IBOutlet weak var settingSlider: UISlider!
-    @IBOutlet weak var settingValue: AGLabel!
-    
-    @IBAction func onSliderValueChanged(sender:UISlider){
-        let val = (sender.value*100).rounded()/100
-        settingValue.text = "\(val)"
-        guard let configs = self.configs as? SettingsSliderParam else {return}
-        delegate?.didChangeValue(key: configs.key, value: val as AnyObject)
-    }
-    
-    override func configure(configs: SettingsBaseParam) {
-        super.configure(configs: configs)
-        
-        guard let param = configs as? SettingsSliderParam else {return}
-        settingLabel.text = param.label
-        settingSlider.value = param.value
-        settingSlider.minimumValue = param.minimumValue
-        settingSlider.maximumValue = param.maximumValue
-        settingValue.text = "\(settingSlider.value)"
-    }
-}
-
-class SettingsSliderParam: SettingsBaseParam {
-    var value:Float
-    var minimumValue:Float
-    var maximumValue:Float
-    init(key:String, label:String, value:Float, minimumValue:Float, maximumValue:Float) {
-        self.value = value
-        self.minimumValue = minimumValue
-        self.maximumValue = maximumValue
-        super.init(key: key, label: label, type: "SliderCell")
-    }
+    func didChangeValue(type:String, key:String, value: Any)
 }
 
 class SettingsViewController : UITableViewController
