34700a6ae0f3261692df61063a95e964cb7d3237
xuchdeid <xuchdeid@gmail.com>
2020-12-16 17:05:33 +0800

    CHG: optimze code

--- macOS/APIExample/Examples/Basic/JoinChannelVideo/JoinChannelVideo.swift
+++ macOS/APIExample/Examples/Basic/JoinChannelVideo/JoinChannelVideo.swift
@@ -57,15 +57,11 @@ class JoinChannelVideoMain: BaseViewController {
         layoutVideos(2)
         
         // prepare resolution picker
-        resolutionPicker.addItems(withTitles: Configs.Resolutions.map({ (res:Resolution) -> String in
-            return res.name()
-        }))
+        resolutionPicker.addItems(withTitles: Configs.Resolutions.map { $0.name() })
         resolutionPicker.selectItem(at: GlobalSettings.shared.resolutionSetting.selectedOption().value)
         
         // prepare fps picker
-        fpsPicker.addItems(withTitles: Configs.Fps.map({ (fps:Int) -> String in
-            return "\(fps)fps"
-        }))
+        fpsPicker.addItems(withTitles: Configs.Fps.map { "\($0)fps" })
         fpsPicker.selectItem(at: GlobalSettings.shared.fpsSetting.selectedOption().value)
         rolePicker.addItems(withTitles: AgoraClientRole.allValues().map({$0.description()}))
         
