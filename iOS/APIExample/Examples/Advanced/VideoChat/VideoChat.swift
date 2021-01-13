cfaa9cc07f6dcc329f3effe186c44cce250b05b5
xuchdeid <xuchdeid@gmail.com>
2021-01-13 16:09:44 +0800

    NEW: Group Video Chat

---
+++ iOS/APIExample/Examples/Advanced/VideoChat/VideoChat.swift
@@ -0,0 +1,302 @@
+//
+//  VideoChat.swift
+//  APIExample
+//
+//  Created by XC on 2021/1/12.
+//  Copyright © 2021 Agora Corp. All rights reserved.
+//
+
+import UIKit
+import AgoraRtcKit
+import AGEVideoLayout
+
+class VideoChatEntry: UIViewController {
+    @IBOutlet weak var joinButton: UIButton!
+    @IBOutlet weak var channelTextField: UITextField!
+    let identifier = "VideoChat"
+    @IBOutlet var resolutionBtn: UIButton!
+    @IBOutlet var fpsBtn: UIButton!
+    @IBOutlet var orientationBtn: UIButton!
+    var width:Int = 640, height:Int = 360, orientation:AgoraVideoOutputOrientationMode = .adaptative, fps = 30
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        resolutionBtn.setTitle("\(width)x\(height)", for: .normal)
+        fpsBtn.setTitle("\(fps)fps", for: .normal)
+        orientationBtn.setTitle("\(orientation.description())", for: .normal)
+    }
+    
+    
+    func getResolutionAction(width:Int, height:Int) -> UIAlertAction{
+        return UIAlertAction(title: "\(width)x\(height)", style: .default, handler: {[unowned self] action in
+            self.width = width
+            self.height = height
+            self.resolutionBtn.setTitle("\(width)x\(height)", for: .normal)
+        })
+    }
+    
+    func getFpsAction(_ fps:Int) -> UIAlertAction{
+        return UIAlertAction(title: "\(fps)fps", style: .default, handler: {[unowned self] action in
+            self.fps = fps
+            self.fpsBtn.setTitle("\(fps)fps", for: .normal)
+        })
+    }
+    
+    func getOrientationAction(_ orientation:AgoraVideoOutputOrientationMode) -> UIAlertAction{
+        return UIAlertAction(title: "\(orientation.description())", style: .default, handler: {[unowned self] action in
+            self.orientation = orientation
+            self.orientationBtn.setTitle("\(orientation.description())", for: .normal)
+        })
+    }
+    
+    @IBAction func setResolution(){
+        let alert = UIAlertController(title: "Set Resolution".localized, message: nil, preferredStyle: .actionSheet)
+        alert.addAction(getResolutionAction(width: 90, height: 90))
+        alert.addAction(getResolutionAction(width: 160, height: 120))
+        alert.addAction(getResolutionAction(width: 320, height: 240))
+        alert.addAction(getResolutionAction(width: 640, height: 360))
+        alert.addAction(getResolutionAction(width: 1280, height: 720))
+        alert.addCancelAction()
+        present(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func setFps(){
+        let alert = UIAlertController(title: "Set Fps".localized, message: nil, preferredStyle: .actionSheet)
+        alert.addAction(getFpsAction(10))
+        alert.addAction(getFpsAction(15))
+        alert.addAction(getFpsAction(24))
+        alert.addAction(getFpsAction(30))
+        alert.addAction(getFpsAction(60))
+        alert.addCancelAction()
+        present(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func setOrientation(){
+        let alert = UIAlertController(title: "Set Orientation".localized, message: nil, preferredStyle: .actionSheet)
+        alert.addAction(getOrientationAction(.adaptative))
+        alert.addAction(getOrientationAction(.fixedLandscape))
+        alert.addAction(getOrientationAction(.fixedPortrait))
+        alert.addCancelAction()
+        present(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func doJoinPressed(sender: UIButton) {
+        guard let channelName = channelTextField.text else {return}
+        //resign channel text field
+        channelTextField.resignFirstResponder()
+        
+        let storyBoard: UIStoryboard = UIStoryboard(name: identifier, bundle: nil)
+        // create new view controller every time to ensure we get a clean vc
+        guard let newViewController = storyBoard.instantiateViewController(withIdentifier: identifier) as? BaseViewController else { return }
+        newViewController.title = channelName
+        newViewController.configs = ["channelName": channelName, "resolution": CGSize(width: width, height: height), "fps": fps, "orientation": orientation]
+        self.navigationController?.pushViewController(newViewController, animated: true)
+    }
+}
+
+class VideoChatMain: BaseViewController {
+    var agoraKit: AgoraRtcEngineKit!
+    @IBOutlet weak var container: AGEVideoContainer!
+    var videoViews: [UInt:VideoView] = [:]
+    
+    // indicate if current instance has joined channel
+    var isJoined: Bool = false
+    
+    override func viewDidLoad(){
+        super.viewDidLoad()
+        
+        // set up agora instance when view loadedlet config = AgoraRtcEngineConfig()
+        let config = AgoraRtcEngineConfig()
+        config.appId = KeyCenter.AppId
+        config.areaCode = GlobalSettings.shared.area
+
+        agoraKit = AgoraRtcEngineKit.sharedEngine(with: config, delegate: self)
+        agoraKit.setLogFile(LogUtils.sdkLogPath())
+        
+        // get channel name from configs
+        guard let channelName = configs["channelName"] as? String,
+            let resolution = configs["resolution"] as? CGSize,
+            let fps = configs["fps"] as? Int,
+            let orientation = configs["orientation"] as? AgoraVideoOutputOrientationMode else { return }
+        // make myself a broadcaster
+        agoraKit.setChannelProfile(.liveBroadcasting)
+        agoraKit.setClientRole(.broadcaster)
+        
+        // enable video module and set up video encoding configs
+        agoraKit.enableVideo()
+        agoraKit.setVideoEncoderConfiguration(
+            AgoraVideoEncoderConfiguration(
+                size: resolution,
+                frameRate: AgoraVideoFrameRate(rawValue: fps) ?? .fps30,
+                bitrate: AgoraVideoBitrateStandard,
+                orientationMode: orientation,
+                mirrorMode: .auto
+            )
+        )
+        
+        // set up local video to render your local camera preview
+        let videoCanvas = AgoraRtcVideoCanvas()
+        videoCanvas.uid = 0
+        
+        let localVideo = Bundle.loadVideoView(type: .local, audioOnly: false)
+        // the view to be binded
+        videoCanvas.view = localVideo.videoView
+        videoCanvas.renderMode = .hidden
+        agoraKit.setupLocalVideo(videoCanvas)
+        
+        videoViews[0] = localVideo
+        container.layoutStream2x2(views: self.sortedViews())
+        // you have to call startPreview to see local video
+        agoraKit.startPreview()
+        // Set audio route to speaker
+        agoraKit.setDefaultAudioRouteToSpeakerphone(true)
+        
+        // start joining channel
+        // 1. Users can only see each other after they join the
+        // same channel successfully using the same app id.
+        // 2. If app certificate is turned on at dashboard, token is needed
+        // when joining channel. The channel name and uid used to calculate
+        // the token has to match the ones used for channel join
+        let option = AgoraRtcChannelMediaOptions()
+        option.clientRoleType = .broadcaster
+        let result = agoraKit.joinChannel(byToken: KeyCenter.Token, channelId: channelName, uid: 0, mediaOptions: option)
+        if result != 0 {
+            // Usually happens with invalid parameters
+            // Error code description can be found at:
+            // en: https://docs.agora.io/en/Voice/API%20Reference/oc/Constants/AgoraErrorCode.html
+            // cn: https://docs.agora.io/cn/Voice/API%20Reference/oc/Constants/AgoraErrorCode.html
+            self.showAlert(title: "Error", message: "joinChannel call failed: \(result), please check your params")
+        }
+    }
+    
+    override func willMove(toParent parent: UIViewController?) {
+        if parent == nil {
+            // leave channel when exiting the view
+            if isJoined {
+                agoraKit.leaveChannel { (stats) -> Void in
+                    LogUtils.log(message: "left channel, duration: \(stats.duration)", level: .info)
+                }
+            }
+        }
+    }
+    
+    func sortedViews() -> [VideoView] {
+        return Array(videoViews.values).sorted(by: { $0.uid < $1.uid })
+    }
+}
+
+/// agora rtc engine delegate events
+extension VideoChatMain: AgoraRtcEngineDelegate {
+    /// callback when warning occured for agora sdk, warning can usually be ignored, still it's nice to check out
+    /// what is happening
+    /// Warning code description can be found at:
+    /// en: https://docs.agora.io/en/Voice/API%20Reference/oc/Constants/AgoraWarningCode.html
+    /// cn: https://docs.agora.io/cn/Voice/API%20Reference/oc/Constants/AgoraWarningCode.html
+    /// @param warningCode warning code of the problem
+    func rtcEngine(_ engine: AgoraRtcEngineKit, didOccurWarning warningCode: AgoraWarningCode) {
+        LogUtils.log(message: "warning: \(warningCode.description)", level: .warning)
+    }
+    
+    /// callback when error occured for agora sdk, you are recommended to display the error descriptions on demand
+    /// to let user know something wrong is happening
+    /// Error code description can be found at:
+    /// en: https://docs.agora.io/en/Voice/API%20Reference/oc/Constants/AgoraErrorCode.html
+    /// cn: https://docs.agora.io/cn/Voice/API%20Reference/oc/Constants/AgoraErrorCode.html
+    /// @param errorCode error code of the problem
+    func rtcEngine(_ engine: AgoraRtcEngineKit, didOccurError errorCode: AgoraErrorCode) {
+        LogUtils.log(message: "error: \(errorCode)", level: .error)
+        self.showAlert(title: "Error", message: "Error \(errorCode.description) occur")
+    }
+    
+    /// callback when the local user joins a specified channel.
+    /// @param channel
+    /// @param uid uid of local user
+    /// @param elapsed time elapse since current sdk instance join the channel in ms
+    func rtcEngine(_ engine: AgoraRtcEngineKit, didJoinChannel channel: String, withUid uid: UInt, elapsed: Int) {
+        isJoined = true
+        LogUtils.log(message: "Join \(channel) with uid \(uid) elapsed \(elapsed)ms", level: .info)
+        //videoViews[0]?.uid = uid
+    }
+    
+    /// callback when a remote user is joinning the channel, note audience in live broadcast mode will NOT trigger this event
+    /// @param uid uid of remote joined user
+    /// @param elapsed time elapse since current sdk instance join the channel in ms
+    func rtcEngine(_ engine: AgoraRtcEngineKit, didJoinedOfUid uid: UInt, elapsed: Int) {
+        LogUtils.log(message: "remote user join: \(uid) \(elapsed)ms", level: .info)
+        
+        let remoteVideo = Bundle.loadVideoView(type: .remote, audioOnly: false)
+        remoteVideo.uid = uid
+        
+        let videoCanvas = AgoraRtcVideoCanvas()
+        videoCanvas.uid = uid
+        // the view to be binded
+        videoCanvas.view = remoteVideo.videoView
+        videoCanvas.renderMode = .hidden
+        agoraKit.setupRemoteVideo(videoCanvas)
+        
+        self.videoViews[uid] = remoteVideo
+        self.container.layoutStream2x2(views: sortedViews())
+        self.container.reload(level: 0, animated: true)
+    }
+    
+    /// callback when a remote user is leaving the channel, note audience in live broadcast mode will NOT trigger this event
+    /// @param uid uid of remote joined user
+    /// @param reason reason why this user left, note this event may be triggered when the remote user
+    /// become an audience in live broadcasting profile
+    func rtcEngine(_ engine: AgoraRtcEngineKit, didOfflineOfUid uid: UInt, reason: AgoraUserOfflineReason) {
+        LogUtils.log(message: "remote user left: \(uid) reason \(reason)", level: .info)
+        
+        let videoCanvas = AgoraRtcVideoCanvas()
+        videoCanvas.uid = uid
+        // the view to be binded
+        videoCanvas.view = nil
+        videoCanvas.renderMode = .hidden
+        agoraKit.setupRemoteVideo(videoCanvas)
+        
+        //remove remote audio view
+        self.videoViews.removeValue(forKey: uid)
+        self.container.layoutStream2x2(views: sortedViews())
+        self.container.reload(level: 0, animated: true)
+    }
+    
+    /// Reports which users are speaking, the speakers' volumes, and whether the local user is speaking.
+    /// @params speakers volume info for all speakers
+    /// @params totalVolume Total volume after audio mixing. The value range is [0,255].
+    func rtcEngine(_ engine: AgoraRtcEngineKit, reportAudioVolumeIndicationOfSpeakers speakers: [AgoraRtcAudioVolumeInfo], totalVolume: Int) {
+//        for volumeInfo in speakers {
+//            if let videoView = videoViews[volumeInfo.uid] {
+//                videoView.setInfo(text: "Volume:\(volumeInfo.volume)")
+//            }
+//        }
+    }
+    
+    /// Reports the statistics of the current call. The SDK triggers this callback once every two seconds after the user joins the channel.
+    /// @param stats stats struct
+    func rtcEngine(_ engine: AgoraRtcEngineKit, reportRtcStats stats: AgoraChannelStats) {
+        videoViews[0]?.statsInfo?.updateChannelStats(stats)
+    }
+    
+    /// Reports the statistics of the uploading local video streams once every two seconds.
+    /// @param stats stats struct
+    func rtcEngine(_ engine: AgoraRtcEngineKit, localVideoStats stats: AgoraRtcLocalVideoStats) {
+        videoViews[0]?.statsInfo?.updateLocalVideoStats(stats)
+    }
+    
+    /// Reports the statistics of the uploading local audio streams once every two seconds.
+    /// @param stats stats struct
+    func rtcEngine(_ engine: AgoraRtcEngineKit, localAudioStats stats: AgoraRtcLocalAudioStats) {
+        videoViews[0]?.statsInfo?.updateLocalAudioStats(stats)
+    }
+    
+    /// Reports the statistics of the video stream from each remote user/host.
+    /// @param stats stats struct
+    func rtcEngine(_ engine: AgoraRtcEngineKit, remoteVideoStats stats: AgoraRtcRemoteVideoStats) {
+        videoViews[stats.uid]?.statsInfo?.updateVideoStats(stats)
+    }
+    
+    /// Reports the statistics of the audio stream from each remote user/host.
+    /// @param stats stats struct for current call statistics
+    func rtcEngine(_ engine: AgoraRtcEngineKit, remoteAudioStats stats: AgoraRtcRemoteAudioStats) {
+        videoViews[stats.uid]?.statsInfo?.updateAudioStats(stats)
+    }
+}
