09c07ab4c6ca3c636ecae5ea1ea5dca2e75df946
xianing <xianing@agora.io>
2021-04-19 20:39:04 -0700

    fix ps script versions

--- windows/APIExample/install.ps1
+++ windows/APIExample/install.ps1
@@ -1,7 +1,7 @@
 $ThirdPartysrc = 'https://agora-adc-artifacts.oss-cn-beijing.aliyuncs.com/libs/ThirdParty.zip'
 $ThirdPartydes = 'ThirdParty.zip'
 $agora_sdk = 'https://download.agora.io/sdk/release/Agora_Native_SDK_for_Windows_v3_4_0_FULL.zip'
-$agora_des = 'Agora_Native_SDK_for_Windows_v3_3_0_FULL.zip'
+$agora_des = 'Agora_Native_SDK_for_Windows.zip'
 $MediaPlayerSDK = 'https://download.agora.io/sdk/release/Agora_Media_Player_for_Windows_x86_32597_20200923_2306.zip'
 $MediaPlayerDes = 'MediaPlayerPartSave.zip'
 
