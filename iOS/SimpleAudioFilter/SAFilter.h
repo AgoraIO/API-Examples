dc4c1af3db3f1ddbca6cc47359935c32f7989c5e
xianing <xianing@agora.io>
2021-07-30 12:11:31 +0800

    update simple audio filter

---
+++ iOS/SimpleAudioFilter/SAFilter.h
@@ -0,0 +1,16 @@
+//
+//  SAFilter.h
+//  SimpleAudioFilter
+//
+//  Created by xianing on 2021/7/29.
+//  Copyright © 2021 Agora Corp. All rights reserved.
+//
+
+#pragma once
+
+#import <AgoraRtcKit/AgoraAudioFilterDelegate.h>
+
+@interface SAFilter : NSObject <AgoraAudioFilterDelegate>
+@end
+
+
