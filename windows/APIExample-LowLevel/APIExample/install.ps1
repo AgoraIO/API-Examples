d9d3abde14bff8de3c4f9964549e3fc92ba1f8ea
sbd021 <sbd021@126.com>
2021-11-22 16:35:23 +0800

    update windows 3.6.200 depenencies and scprit

--- windows/APIExample-LowLevel/APIExample/install.ps1
+++ windows/APIExample-LowLevel/APIExample/install.ps1
@@ -3,16 +3,6 @@ $ThirdPartydes = 'ThirdParty.zip'
 $agora_sdk = 'https://github.com/AgoraIO/API-Examples/releases/download/3.4.5/3.6.200_x86_windows_full.zip'
 $agora_des = '3.6.200_x86_windows_full.zip'
 
-if (-not (Test-Path ThirdParty)){
-	echo "download $ThirdPartydes"
-	mkdir ThirdParty
-	(New-Object System.Net.WebClient).DownloadFile($ThirdPartySrc,$ThirdPartyDes)
-	Unblock-File $ThirdPartyDes
-	Expand-Archive -Path $ThirdPartyDes -DestinationPath 'ThirdParty' -Force 
-	Remove-Item $ThirdPartyDes -Recurse
-}
-
-
 if (-not (Test-Path sdk)){
 	echo "download $agora_des"
 	mkdir sdk
