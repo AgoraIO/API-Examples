0a30d18a84ef7a422bf602b2431ff56337d6784f
xianing <xianing@agora.io>
2021-12-17 11:50:16 +0800

    use texture helper for custom video source

--- Android/APIExample/app/src/main/java/io/agora/api/example/examples/advanced/PushExternalVideo.java
+++ Android/APIExample/app/src/main/java/io/agora/api/example/examples/advanced/PushExternalVideo.java
@@ -347,14 +347,14 @@ public class PushExternalVideo extends BaseFragment implements View.OnClickListe
 
         if (joined) {
             VideoFrame.Buffer buffer = textureBufferHelper.invoke(new Callable<VideoFrame.Buffer>() {
-                      @Override
-                      public VideoFrame.Buffer call() throws Exception
-                      {
-                          return textureBufferHelper.wrapTextureBuffer( DEFAULT_CAPTURE_HEIGHT,
-                                  DEFAULT_CAPTURE_WIDTH, VideoFrame.TextureBuffer.Type.OES, mPreviewTexture,
-                                  RendererCommon.convertMatrixToAndroidGraphicsMatrix(mTransform));
-                      }
-                  });
+                @Override
+                public VideoFrame.Buffer call() throws Exception
+                {
+                    return textureBufferHelper.wrapTextureBuffer( DEFAULT_CAPTURE_HEIGHT,
+                            DEFAULT_CAPTURE_WIDTH, VideoFrame.TextureBuffer.Type.OES, mPreviewTexture,
+                            RendererCommon.convertMatrixToAndroidGraphicsMatrix(mTransform));
+                }
+            });
             VideoFrame frame = new VideoFrame(buffer, 0, System.nanoTime());
             /**Pushes the video frame using the AgoraVideoFrame class and passes the video frame to the Agora SDK.
              * Call the setExternalVideoSource method and set pushMode as true before calling this
@@ -366,7 +366,7 @@ public class PushExternalVideo extends BaseFragment implements View.OnClickListe
              * PS:
              *   In the Communication profile, the SDK does not support textured video frames.*/
             boolean a = engine.pushExternalVideoFrame(frame);
-            Log.i(TAG, "pushExternalVideoFrame:" + a);
+            Log.d(TAG, "pushExternalVideoFrame:" + a);
         }
     }
 
