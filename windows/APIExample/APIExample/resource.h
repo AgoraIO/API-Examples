06ded890116e7aafb5bd24c562e39ed7344cde9d
sbd021 <sbd021@126.com>
2022-01-20 17:34:51 +0800

    report nework data

--- windows/APIExample/APIExample/resource.h
+++ windows/APIExample/APIExample/resource.h
@@ -1,6 +1,6 @@
 //{{NO_DEPENDENCIES}}
-// Microsoft Visual C++ generated include file.
-// Used by APIExample.rc
+// Microsoft Visual C++ 生成的包含文件。
+// 供 APIExample.rc 使用
 //
 #define IDM_ABOUTBOX                    0x0010
 #define IDD_ABOUTBOX                    100
@@ -301,6 +301,8 @@
 #define IDC_EDIT1                       1179
 #define IDC_EDIT_IMAGE_PATH             1180
 #define IDC_BUTTON_ECHO_TEST2           1181
+#define IDC_CHECK_                      1182
+#define IDC_CHECK_REPORT                1182
 
 // Next default values for new objects
 // 
@@ -308,7 +310,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        139
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1182
+#define _APS_NEXT_CONTROL_VALUE         1183
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
