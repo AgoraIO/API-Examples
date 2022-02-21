a53d74f5c557586ee9887622d15dcc9c60e137cf
sbd021 <sbd021@126.com>
2022-02-21 16:54:52 +0800

    windows multiprocess 64 and update readme

---
+++ windows/APIExample/installThirdParty_x64.bat
@@ -0,0 +1,10 @@
+cd /d %~dp0
+
+if not exist .\libs (
+    if exist ..\..\..\libs (
+        echo d | xcopy ..\..\..\libs .\libs /Y /E /Q
+    )
+)
+
+powershell.exe -command ^
+  "& {set-executionpolicy Remotesigned -Scope Process; ./'install.ps1' 'x64'}"
