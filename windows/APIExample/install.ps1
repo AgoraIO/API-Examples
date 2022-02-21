cf70624e8a69a2fd7503f99a56ba52e2510ddcb5
sbd021 <sbd021@126.com>
2022-02-21 18:02:17 +0800

    windows 37200 x64

--- windows/APIExample/install.ps1
+++ windows/APIExample/install.ps1
@@ -1,7 +1,7 @@
 $ThirdPartysrc = 'https://agora-adc-artifacts.oss-cn-beijing.aliyuncs.com/libs/ThirdParty.zip'
 $ThirdPartydes = 'ThirdParty.zip'
-$agora_sdk = 'https://download.agora.io/sdk/release/Agora_Native_SDK_for_Windows_x86_rel.v3.7.201_55354_20211231_1714_full.zip'
-$agora_des = 'Agora_Native_SDK_for_Windows_x86_rel.v3.7.201_55354_20211231_1714_full.zip'
+$agora_sdk = 'https://download.agora.io/sdk/release/Agora_Native_SDK_for_Windows_v4.0.0.preview.5_full.zip'
+$agora_des = 'AgoraSdk.zip'
 
 if (-not (Test-Path ThirdParty)){
 	echo "download $ThirdPartydes"
@@ -16,12 +16,15 @@ if (-not (Test-Path ThirdParty)){
 if (-not (Test-Path sdk)){
 	echo "download $agora_des"
 	mkdir sdk
+	mkdir sdk/x86
+	mkdir sdk/x64
 	(New-Object System.Net.WebClient).DownloadFile($agora_sdk,$agora_des)
 	Unblock-File $agora_des
 	Expand-Archive -Path $agora_des -DestinationPath . -Force 
-	Move-Item Agora_Native_SDK_for_Windows_x86_rel.v3.7.201_55354_20211231_1714_full\rtc\sdk\*  sdk
+	Move-Item Agora_Native_SDK_for_Windows_FULL\x86\rtc\sdk\*  sdk\x86
+	Move-Item Agora_Native_SDK_for_Windows_FULL\x86_64\rtc\sdk\*  sdk\x64
 	Remove-Item  $agora_des -Recurse
-	Remove-Item  Agora_Native_SDK_for_Windows_x86_rel.v3.7.201_55354_20211231_1714_full -Recurse
+	Remove-Item  Agora_Native_SDK_for_Windows_FULL -Recurse
 }
 
 
