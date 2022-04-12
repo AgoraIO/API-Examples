ae3afd7c1b49eca79e4b017e155f08b9d25aed25
Arlin <lichenglin@agora.io>
2022-04-12 22:57:22 +0800

    Add RawVideoData base v3.8.200 by mac

---
+++ macOS/APIExample/Commons/Utils/MediaUtils.h
@@ -0,0 +1,20 @@
+//
+//  MediaUtils.h
+//  APIExample
+//
+//  Created by Arlin on 2022/4/12.
+//  Copyright © 2022 Agora Corp. All rights reserved.
+//
+
+//#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MediaUtils : NSObject
+
++ (nullable NSImage *)i420ToImage:(nullable void *)srcY srcU:(nullable void *)srcU srcV:(nullable void *)srcV width:(int)width height:(int)height;
+
+@end
+
+NS_ASSUME_NONNULL_END
