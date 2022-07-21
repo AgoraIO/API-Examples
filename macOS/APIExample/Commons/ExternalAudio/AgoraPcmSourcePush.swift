3c808b0556241aa7272bdd89cada1ad69017ad96
zhaoyongqiang <zhaoyongqiang@agora.io>
2022-07-21 14:11:41 +0800

    fix MS-202875 Mac播放pcm没有声音的问题

--- macOS/APIExample/Commons/ExternalAudio/AgoraPcmSourcePush.swift
+++ macOS/APIExample/Commons/ExternalAudio/AgoraPcmSourcePush.swift
@@ -19,11 +19,7 @@ class AgoraPcmSourcePush: NSObject {
     private let channelsPerFrame: Int
     private let bitPerSample: Int
     private let samples: Int
-    private var audioTimer: DispatchSourceTimer?
-    private var audioSentFrames: Int64 = 0
-    private var audioSendStartMS: Int64 = 0
-    private var input: InputStream?
-    private var buffer: UnsafeMutablePointer<UInt8>?
+    
     private var state: State = .Stop
     private let captureQueue: DispatchQueue
 
@@ -41,7 +37,6 @@ class AgoraPcmSourcePush: NSObject {
         self.samples = samples
         captureQueue = DispatchQueue(label: "MyCaptureQueue")
     }
-
     
     func start() {
         if state == .Stop {
@@ -54,75 +49,31 @@ class AgoraPcmSourcePush: NSObject {
         if state == .Play {
             state = .Stop
         }
-      audioSentFrames = 0
-      audioSendStartMS = 0
-      if let buffer = buffer, let input = input {
-        buffer.deallocate()
-        input.close()
-      }
-      buffer = nil
-      input = nil
-      guard let timer = audioTimer else {
-          return
-      }
-      timer.cancel()
-      
-      audioTimer = nil
-      return
     }
 
-//    private func play() {
-//        captureQueue.async {
-//            guard let input = InputStream(fileAtPath: self.filePath) else { return }
-//            input.open()
-//
-//            let bufferSize = self.samples * self.bitPerSample / 8 * self.channelsPerFrame
-//            let sleepTime = TimeInterval(self.samples) / TimeInterval(self.sampleRate)
-//            let buffer = UnsafeMutablePointer<UInt8>.allocate(capacity: bufferSize)
-//            while input.hasBytesAvailable && self.state == .Play {
-//                input.read(buffer, maxLength: bufferSize)
-//                self.delegate?.onAudioFrame(data: Data(bytes: buffer, count: bufferSize))
-//                Thread.sleep(forTimeInterval: sleepTime)
-//            }
-//            buffer.deallocate()
-//            input.close()
-//        }
-//    }
-  
-  private func currentTimeMillis() -> Int64 {
-     var darwinTime : timeval = timeval(tv_sec: 0, tv_usec: 0)
-     gettimeofday(&darwinTime, nil)
-     return (Int64(darwinTime.tv_sec) * 1000) + Int64(darwinTime.tv_usec / 1000)
-  }
-  
-  func play() {
-      let bufferSize = self.samples * self.bitPerSample / 8 * self.channelsPerFrame
-      input = InputStream(fileAtPath: filePath)
-      input?.open()
-      buffer = UnsafeMutablePointer<UInt8>.allocate(capacity: bufferSize)
-      guard let input = input, let buffer = buffer  else { return }
-      let timer = DispatchSource.makeTimerSource(queue: captureQueue)
-      timer.schedule(wallDeadline: .now(), repeating: 0.1)
-      
-      timer.setEventHandler { [weak self] in
-          guard let self = self else {
-              return
-          }
-          
-          if (self.audioSentFrames == 0) {
-              self.audioSendStartMS = self.currentTimeMillis()
-          }
-          
-          let currentMS = self.currentTimeMillis()
-          
-        let targetSentFrames = ((currentMS - self.audioSendStartMS) / 100) + 1;
-        while (targetSentFrames > self.audioSentFrames && (input.hasBytesAvailable && self.state == .Play)) {
-//          let size = input.read(buffer, maxLength: bufferSize)
-          self.delegate?.onAudioFrame(data:buffer)
-          self.audioSentFrames += 1
+    private func play() {
+        captureQueue.async {
+            guard let input = InputStream(fileAtPath: self.filePath) else { return }
+            input.open()
+                        
+            let bufferSize = self.samples * self.bitPerSample / 8 * self.channelsPerFrame
+            let sleepTime = TimeInterval(self.samples) / TimeInterval(self.sampleRate)
+            let buffer = UnsafeMutablePointer<UInt8>.allocate(capacity: bufferSize)
+            var index: Double = 0
+            var firstPush: Double = 0
+            while input.hasBytesAvailable && self.state == .Play {
+                if firstPush == 0 {
+                    firstPush = CFAbsoluteTimeGetCurrent()
+                }
+                input.read(buffer, maxLength: bufferSize)
+                self.delegate?.onAudioFrame(data: buffer)
+                let nextPush = firstPush + index * sleepTime
+                let sleep = nextPush - CFAbsoluteTimeGetCurrent()
+                Thread.sleep(forTimeInterval: sleep)
+                index += 1
+            }
+            buffer.deallocate()
+            input.close()
         }
-      }
-      timer.resume()
-      audioTimer = timer
-  }
+    }
 }
