4748821ccfa0db33668f21b9155d1e4f435d3184
xucz <xgfd3@users.noreply.github.com>
2022-08-25 15:05:30 +0800

    [windows]update ci config.

--- .github/ci/build/build_windows.bat
+++ .github/ci/build/build_windows.bat
@@ -79,6 +79,8 @@ del /F /Q Agora_Native_SDK_for_Windows_FULL\commits
 del /F /Q Agora_Native_SDK_for_Windows_FULL\package_size_report.txt
 mkdir Agora_Native_SDK_for_Windows_FULL\samples
 mkdir Agora_Native_SDK_for_Windows_FULL\samples\API-example
-xcopy /Y /E windows Agora_Native_SDK_for_Windows_FULL\samples\API-example
+xcopy /Y /E windows\APIExample Agora_Native_SDK_for_Windows_FULL\samples\API-example
+xcopy /Y /E windows\README.md Agora_Native_SDK_for_Windows_FULL\samples\API-example
+xcopy /Y /E windows\README.zh.md Agora_Native_SDK_for_Windows_FULL\samples\API-example
 7z a -tzip result.zip -r Agora_Native_SDK_for_Windows_FULL
 copy result.zip %WORKSPACE%\\withAPIExample_%zip_name%
