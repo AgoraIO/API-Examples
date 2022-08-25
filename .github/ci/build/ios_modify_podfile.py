818cc590b89ffb7ae2f54a7434f0a4a00293761d
zhaoyongqiang <zhaoyongqiang@agora.io>
2022-08-25 16:23:39 +0800

    修改脚本

--- .github/ci/build/ios_modify_podfile.py
+++ .github/ci/build/ios_modify_podfile.py
@@ -4,6 +4,8 @@ def modify_podfile(path: str):
 	with open(path, 'r', encoding='utf-8') as f:
 		content = f.read()
 		content = content.replace("pod 'sdk', :path => 'sdk.podspec'", "pod 'sdk', :path => '../../sdk.podspec'")
+		content = content.replace("pod 'Agora", "#pod 'Agora")
+			
 		f.close()
 		with open(path, 'w', encoding='utf-8') as fw:
 			fw.write(content)
