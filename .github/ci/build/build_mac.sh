f52ecab2f7b4c74662aa75ee28a8432193eb2801
cleven <543069316@qq.com>
2022-09-20 12:47:01 +0800

    Update build_mac.sh

--- .github/ci/build/build_mac.sh
+++ .github/ci/build/build_mac.sh
@@ -81,7 +81,7 @@ gsed -i "s|pod 'Agora|#pod 'Agora|" ./$unzip_name/samples/APIExample/Podfile
 #     exit 1
 # fi
 
-./.github/ci/build/build_mac_ipa.sh ./$unzip_name/samples/APIExample
+# ./.github/ci/build/build_mac_ipa.sh ./$unzip_name/samples/APIExample
 
 7za a -tzip result.zip -r $unzip_name
 # 7za a -tzip result.zip -r Agora_Native_SDK_for_Mac_FULL
