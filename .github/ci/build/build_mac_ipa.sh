fc27d6330d8048da766eb2734692c99291ffd87d
cleven <543069316@qq.com>
2022-09-21 16:24:06 +0800

    Update build_mac_ipa.sh

--- .github/ci/build/build_mac_ipa.sh
+++ .github/ci/build/build_mac_ipa.sh
@@ -58,7 +58,7 @@ echo KEYCENTER_PATH: $KEYCENTER_PATH
 echo APP_PATH: $APP_PATH
 
 #修改Keycenter文件
-python3 ./.github/ci/build/modify_ios_keycenter $KEYCENTER_PATH 0 $AGORA_APP_ID
+python3 ./.github/ci/build/modify_ios_keycenter.py $KEYCENTER_PATH 0 $AGORA_APP_ID
 if [ $? -eq 0 ]; then
     echo "修改Keycenter文件 success"
 else
@@ -91,7 +91,7 @@ xcodebuild -exportArchive -archivePath "${ARCHIVE_PATH}" -exportPath "${EXPORT_P
 
 
 #复原Keycenter文件
-python3 ./.github/ci/build/modify_ios_keycenter $KEYCENTER_PATH 1 $AGORA_APP_ID
+python3 ./.github/ci/build/modify_ios_keycenter.py $KEYCENTER_PATH 1 $AGORA_APP_ID
 if [ $? -eq 0 ]; then
     echo "复原Keycenter文件 success"
 else
