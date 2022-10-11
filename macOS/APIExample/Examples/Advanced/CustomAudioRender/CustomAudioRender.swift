db2a180c6776adac8c4bdef0a395fd3f6f759da1
zhaoyongqiang <zhaoyongqiang@agora.io>
2022-10-11 16:16:10 +0800

    Mac add a token verification module

--- macOS/APIExample/Examples/Advanced/CustomAudioRender/CustomAudioRender.swift
+++ macOS/APIExample/Examples/Advanced/CustomAudioRender/CustomAudioRender.swift
@@ -182,8 +182,8 @@ class CustomAudioRender: BaseViewController {
             let option = AgoraRtcChannelMediaOptions()
             option.publishCameraTrack = true
             option.clientRoleType = .broadcaster
-            NetworkManager.shared.generateToken(channelName: channel) {
-                let result = self.agoraKit.joinChannel(byToken: KeyCenter.Token, channelId: channel, uid: 0, mediaOptions: option)
+            NetworkManager.shared.generateToken(channelName: channel, success: { token in
+                let result = self.agoraKit.joinChannel(byToken: token, channelId: channel, uid: 0, mediaOptions: option)
                 if result != 0 {
                     self.isProcessing = false
                     // Usually happens with invalid parameters
@@ -192,7 +192,7 @@ class CustomAudioRender: BaseViewController {
                     // cn: https://docs.agora.io/cn/Voice/API%20Reference/oc/Constants/AgoraErrorCode.html
                     self.showAlert(title: "Error", message: "joinChannel call failed: \(result), please check your params")
                 }
-            }
+            })
         } else {
             isProcessing = true
             agoraKit.leaveChannel { (stats:AgoraChannelStats) in
