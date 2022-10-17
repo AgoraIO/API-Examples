d8845bf9d277c5b537735c2ed725cb55f5f917ec
xucz <xgfd3@users.noreply.github.com>
2022-10-17 16:55:45 +0800

    [windows]update MultiChannel impl and add leaveChannelEx option.

--- windows/APIExample/APIExample/stdafx.cpp
+++ windows/APIExample/APIExample/stdafx.cpp
@@ -194,6 +194,9 @@ wchar_t MeidaPlayerCtrlUnPublishAudio[INFO_LEN] = { 0 };
 
 //multi channel
 wchar_t MultiChannelCtrlChannelList[INFO_LEN] = { 0 };
+wchar_t MultiChannelCtrlJoinExChannel[INFO_LEN] = { 0 };
+wchar_t MultiChannelCtrlLeaveExChannel[INFO_LEN] = { 0 };
+wchar_t MultiChannelCtrlStopMic[INFO_LEN] = { 0 };
 
 
 //audio effect
@@ -624,6 +627,9 @@ void InitKeyInfomation()
 
 	//multichannel
 	_tcscpy_s(MultiChannelCtrlChannelList, INFO_LEN, Str(_T("MultiChannel.Ctrl.ChannelList")));
+	_tcscpy_s(MultiChannelCtrlJoinExChannel, INFO_LEN, Str(_T("MultiChannel.Ctrl.JoinExChannel")));
+	_tcscpy_s(MultiChannelCtrlLeaveExChannel, INFO_LEN, Str(_T("MultiChannel.Ctrl.LeaveExChannel")));
+	_tcscpy_s(MultiChannelCtrlStopMic, INFO_LEN, Str(_T("MultiChannel.Ctrl.StopMic")));
 
 
 	_tcscpy_s(PerCallTestCtrlAudioInput, INFO_LEN, Str(_T("PerCallTest.Ctrl.AudioInput")));
