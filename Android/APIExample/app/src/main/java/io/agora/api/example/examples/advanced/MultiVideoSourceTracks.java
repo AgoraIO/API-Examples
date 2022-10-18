3b20a876ff0334380188334c0a30523be63fc7bb
xucz <xgfd3@users.noreply.github.com>
2022-10-18 14:51:59 +0800

    [Android] add private cloud config.

--- Android/APIExample/app/src/main/java/io/agora/api/example/examples/advanced/MultiVideoSourceTracks.java
+++ Android/APIExample/app/src/main/java/io/agora/api/example/examples/advanced/MultiVideoSourceTracks.java
@@ -133,6 +133,8 @@ public class MultiVideoSourceTracks extends BaseFragment implements View.OnClick
                     + "\"appVersion\":\"" + RtcEngine.getSdkVersion() + "\""
                     + "}"
                     + "}");
+            /* setting the local access point if the private cloud ip was set, otherwise the config will be invalid.*/
+            engine.setLocalAccessPoint(((MainApplication) getActivity().getApplication()).getGlobalSettings().getPrivateCloudConfig());
         } catch (Exception e) {
             e.printStackTrace();
         }
