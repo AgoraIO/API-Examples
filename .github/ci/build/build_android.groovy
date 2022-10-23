7844e8cb00339b773e08b37ae793a3f55614f8df
xucz <xgfd3@users.noreply.github.com>
2022-10-23 17:19:58 +0800

    [Android] update ci.

--- .github/ci/build/build_android.groovy
+++ .github/ci/build/build_android.groovy
@@ -7,7 +7,7 @@ buildUtils = new agora.build.BuildUtils()
 
 compileConfig = [
     "sourceDir": "api-examples",
-    "docker": "hub.agoralab.co/server/build_apiexample:1.0",
+    "docker": "hub.agoralab.co/server/android_jdk11:latest",
     "non-publish": [
         "command": "./.github/ci/build/build_android.sh",
         "extraArgs": "",
