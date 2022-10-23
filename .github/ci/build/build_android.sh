584074acec6bd6577afda9cf03498e4163ba99bc
xucz <xgfd3@users.noreply.github.com>
2022-10-23 20:45:17 +0800

    [Android] update ci 9 -- docker

--- .github/ci/build/build_android.sh
+++ .github/ci/build/build_android.sh
@@ -55,7 +55,7 @@ export PYTHONIOENCODING=utf8
 zip_name=${sdk_url##*/}
 echo zip_name: $zip_name
 
-python3 $WORKSPACE/artifactory_utils.py --action=download_file --file=$sdk_url
+env LC_ALL=en_US.UTF-8 python3 $WORKSPACE/artifactory_utils.py --action=download_file --file=$sdk_url || exit 1
 7za x ./$zip_name -y
 
 unzip_name=`ls -S -d */ | grep Agora | sed 's/\///g'`
