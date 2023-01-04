61d2d06c7a75cd9a6100a0f71a6a1eae0fd3f616
zhaoyongqiang <zhaoyongqiang@agora.io>
2023-01-04 11:26:51 +0800

    audio update default resolution params

--- iOS/APIExample-Audio/APIExample-Audio/Common/GlobalSettings.swift
+++ iOS/APIExample-Audio/APIExample-Audio/Common/GlobalSettings.swift
@@ -49,10 +49,10 @@ class GlobalSettings {
             SettingItemOption(idx: 0, label: "90x90", value: CGSize(width: 90, height: 90)),
             SettingItemOption(idx: 1, label: "160x120", value: CGSize(width: 160, height: 120)),
             SettingItemOption(idx: 2, label: "320x240", value: CGSize(width: 320, height: 240)),
-            SettingItemOption(idx: 3, label: "640x360", value: CGSize(width: 640, height: 360)),
+            SettingItemOption(idx: 3, label: "640x360", value: CGSize(width: 960, height: 540)),
             SettingItemOption(idx: 4, label: "1280x720", value: CGSize(width: 1280, height: 720))
         ]),
-        "fps": SettingItem(selected: 3, options: [
+        "fps": SettingItem(selected: 1, options: [
             SettingItemOption(idx: 0, label: "10fps", value: AgoraVideoFrameRate.fps10),
             SettingItemOption(idx: 1, label: "15fps", value: AgoraVideoFrameRate.fps15),
             SettingItemOption(idx: 2, label: "24fps", value: AgoraVideoFrameRate.fps24),
