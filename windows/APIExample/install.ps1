f61e3161b6db8145ad3a38f38ad74f81c23e3a43
xucz <xucom786647379@gmail.com>
2023-06-25 16:04:47 +0800

    [windows]update rtc sdk url.

--- windows/APIExample/install.ps1
+++ windows/APIExample/install.ps1
@@ -1,6 +1,6 @@
 $ThirdPartysrc = 'https://download.agora.io/staging/ThirdParty.zip'
 $ThirdPartydes = 'ThirdParty.zip'
-$agora_sdk = 'https://download.agora.io/sdk/release/Agora_Native_SDK_for_Windows_v4.2.1_FULL.zip'
+$agora_sdk = 'https://download.agora.io/sdk/release/Agora_Native_SDK_for_Windows_v4.2.2_FULL.zip'
 $agora_des = 'AgoraSdk.zip'
 $agora_local_sdk = '../../sdk'
 
