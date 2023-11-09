da2fafdcf37dc2ad4a43d74c3ca63372825305eb
xucz <xucom786647379@gmail.com>
2023-11-09 14:38:56 +0800

    [Winodws]fix some crashes(NMS-17129/NMS-17191)

--- windows/APIExample/APIExample/Advanced/PushExternalVideoYUV/PushExternalVideoYUV.cpp
+++ windows/APIExample/APIExample/Advanced/PushExternalVideoYUV/PushExternalVideoYUV.cpp
@@ -120,7 +120,11 @@ void PushExternalVideoYUV::UnInitAgora()
 {
 	if (m_rtcEngine) {
 		yuvReader.stop();
-		delete m_imgBuffer;
+		if (m_imgBuffer) {
+			delete m_imgBuffer;
+			m_imgBuffer = nullptr;
+		}
+		
 
 		if (m_joinChannel)
 			m_joinChannel = !m_rtcEngine->leaveChannel();
@@ -134,8 +138,10 @@ void PushExternalVideoYUV::UnInitAgora()
 		m_rtcEngine->disableVideo();
 		m_lstInfo.InsertString(m_lstInfo.GetCount(), _T("disableVideo"));
 		//release engine.
-		m_rtcEngine->release(true);
-		m_lstInfo.InsertString(m_lstInfo.GetCount(), _T("release rtc engine"));
+		if (m_initialize) {
+			m_rtcEngine->release(true);
+			m_lstInfo.InsertString(m_lstInfo.GetCount(), _T("release rtc engine"));
+		}
 		m_rtcEngine = NULL;
 	}
 }
--- windows/APIExample/APIExample/Advanced/RTMPStream/AgoraRtmpStreaming.cpp
+++ windows/APIExample/APIExample/Advanced/RTMPStream/AgoraRtmpStreaming.cpp
@@ -248,8 +248,10 @@ void CAgoraRtmpStreamingDlg::UnInitAgora()
 		m_rtcEngine->disableVideo();
 		m_lstInfo.InsertString(m_lstInfo.GetCount(), _T("disableVideo"));
 		//release engine.
-		m_rtcEngine->release(true);
-		m_lstInfo.InsertString(m_lstInfo.GetCount(), _T("release rtc engine"));
+		if (m_initialize) {
+			m_rtcEngine->release(true);
+			m_lstInfo.InsertString(m_lstInfo.GetCount(), _T("release rtc engine"));
+		}
 		m_rtcEngine = NULL;
 	}
 }
