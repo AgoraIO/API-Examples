cb080da513e2986d79f99b26bc6f93206ba2e9e6
sync2gitee <sync2gitee@example.com>
2024-03-08 16:38:44 +0800

    fix sync shell

--- .github/workflows/gitee-sync-shell.sh
+++ .github/workflows/gitee-sync-shell.sh
@@ -4,7 +4,6 @@ git config --global user.name "sync2gitee"
 
 pwd
 git remote -v
-git checkout dev/4.3.1
 
 # change android maven to china repos
 sed -ie "s#google()#maven { url \"https\://maven.aliyun.com/repository/public\" }\n        google()#g" Android/APIExample/settings.gradle
@@ -15,10 +14,10 @@ git add Android/APIExample/settings.gradle Android/APIExample/gradle/wrapper/gra
 git commit -m '[Android] gitee sync >> use china repos.'
 
 # change iOS Podfile to china repos
-sed -ie '1s/^/source "https://mirrors.tuna.tsinghua.edu.cn/git/CocoaPods/Specs.git"\n' iOS/APIExample/Podfile
-sed -ie '1s/^/source "https://mirrors.tuna.tsinghua.edu.cn/git/CocoaPods/Specs.git"\n' iOS/APIExample-Audio/Podfile
-sed -ie '1s/^/source "https://mirrors.tuna.tsinghua.edu.cn/git/CocoaPods/Specs.git"\n' iOS/APIExample-OC/Podfile
-sed -ie '1s/^/source "https://mirrors.tuna.tsinghua.edu.cn/git/CocoaPods/Specs.git"\n' macOS/Podfile
+sed -ie '1s#^#source "https://mirrors.tuna.tsinghua.edu.cn/git/CocoaPods/Specs.git"\n#' iOS/APIExample/Podfile
+sed -ie '1s#^#source "https://mirrors.tuna.tsinghua.edu.cn/git/CocoaPods/Specs.git"\n#' iOS/APIExample-Audio/Podfile
+sed -ie '1s#^#source "https://mirrors.tuna.tsinghua.edu.cn/git/CocoaPods/Specs.git"\n#' iOS/APIExample-OC/Podfile
+sed -ie '1s#^#source "https://mirrors.tuna.tsinghua.edu.cn/git/CocoaPods/Specs.git"\n#' macOS/Podfile
 git add iOS/APIExample/Podfile iOS/APIExample-Audio/Podfile iOS/APIExample-OC/Podfile macOS/Podfile
 git commit -m '[iOS] gitee sync >> use china repos.'
 
