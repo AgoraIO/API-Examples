3fce1df42cc4553397c9f050f693407a77f62bed
xucz <xucom786647379@gmail.com>
2024-05-24 13:59:37 +0800

    [Android]Update ci compile script.

--- Android/APIExample/cloud_build.sh
+++ Android/APIExample/cloud_build.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env sh
 
+# cache gradle to /tmp/.gradle
+ls ~/.gradle || (mkdir -p /tmp/.gradle && ln -s /tmp/.gradle ~/.gradle && touch ~/.gradle/ln_$(date "+%y%m%d%H") && ls ~/.gradle)
+
 ## config appId
 sed -i -e "s#YOUR APP ID#${APP_ID}#g" app/src/main/res/values/string_configs.xml
 sed -i -e "s#YOUR APP CERTIFICATE##g" app/src/main/res/values/string_configs.xml
