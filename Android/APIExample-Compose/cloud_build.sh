606e79a5cd405afbb496a5dd7d266b03bc55b472
xucz <xucom786647379@gmail.com>
2024-05-24 16:35:30 +0800

    [Android][Compose]update cloud build script.

--- Android/APIExample-Compose/cloud_build.sh
+++ Android/APIExample-Compose/cloud_build.sh
@@ -6,7 +6,7 @@ ls ~/.gradle || (mkdir -p /tmp/.gradle && ln -s /tmp/.gradle ~/.gradle && touch 
 ## use open jdk 17
 if [ ! -d "/tmp/jdk-17" ];then
   mkdir -p /tmp/jdk-17
-  wget https://download.java.net/java/GA/jdk17.0.2/dfd4a8d0985749f896bed50d7138ee7f/8/GPL/openjdk-17.0.2_linux-x64_bin.tar.gz
+  curl -O https://download.java.net/java/GA/jdk17.0.2/dfd4a8d0985749f896bed50d7138ee7f/8/GPL/openjdk-17.0.2_linux-x64_bin.tar.gz
   tar xvf openjdk-17.0.2_linux-x64_bin.tar.gz
   mv jdk-17.0.2/ /tmp/jdk-17/
 fi
