ff792dbc250ac612bb8dcaa803bd7eb41dc7ca16
xucz <xucom786647379@gmail.com>
2024-05-24 16:46:10 +0800

    [Android][Compose]update cloud build script.

--- Android/APIExample-Compose/cloud_build.sh
+++ Android/APIExample-Compose/cloud_build.sh
@@ -4,15 +4,16 @@
 ls ~/.gradle || (mkdir -p /tmp/.gradle && ln -s /tmp/.gradle ~/.gradle && touch ~/.gradle/ln_$(date "+%y%m%d%H") && ls ~/.gradle)
 
 ## use open jdk 17
-if [ ! -d "/tmp/jdk-17/17.0.2" ];then
-  mkdir -p /tmp/jdk-17/17.0.2
+if [ "${$(uname -s): 0: 4}" = "Linu" ];then
+if [ ! -d "/tmp/jdk-17.0.2" ];then
   curl -O https://download.java.net/java/GA/jdk17.0.2/dfd4a8d0985749f896bed50d7138ee7f/8/GPL/openjdk-17.0.2_linux-x64_bin.tar.gz
-  7za x openjdk-17.0.2_linux-x64_bin.tar.gz -y > log.txt
-  mv jdk-17.0.2/ /tmp/jdk-17/17.0.2
+  tar zxf openjdk-17.0.2_linux-x64_bin.tar.gz
+  mv jdk-17.0.2 /tmp/
 fi
-export JAVA_HOME=/tmp/jdk-17/17.0.2
+export JAVA_HOME=/tmp/jdk-17.0.2
 export PATH=$JAVA_HOME/bin:$PATH
 java --version
+fi
 
 ## config appId
 if [ ! -f "local.properties" ];then
