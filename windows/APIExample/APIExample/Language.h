b1ebaad3e699e684a70fd84657fd99c9bff860a3
1138289316@qq.com <1138289316@qq.com>
2024-09-11 14:53:44 +0800

    feat: 增加了透明背景交互入口

--- windows/APIExample/APIExample/Language.h
+++ windows/APIExample/APIExample/Language.h
@@ -348,3 +348,6 @@ extern wchar_t localVideoTranscodingVirtualBg[INFO_LEN];
 extern wchar_t advancedMetadataVideo[INFO_LEN];
 extern wchar_t advancedMetadataAudio[INFO_LEN];
 extern wchar_t advancedMetadataSend[INFO_LEN];
+
+//transparent background
+extern wchar_t TransparentBackground[INFO_LEN];
