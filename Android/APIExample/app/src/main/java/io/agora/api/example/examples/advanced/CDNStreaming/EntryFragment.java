3547425ec6578e202a07ada502e0b4c56ce286f1
weileifrank <1138289316@qq.com>
2024-09-30 16:49:24 +0800

    feat: update sdk to 4.5.0

--- Android/APIExample/app/src/main/java/io/agora/api/example/examples/advanced/CDNStreaming/EntryFragment.java
+++ Android/APIExample/app/src/main/java/io/agora/api/example/examples/advanced/CDNStreaming/EntryFragment.java
@@ -14,12 +14,10 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.navigation.Navigation;
 
-import com.yanzhenjie.permission.AndPermission;
-import com.yanzhenjie.permission.runtime.Permission;
-
 import io.agora.api.example.R;
 import io.agora.api.example.annotation.Example;
 import io.agora.api.example.common.BaseFragment;
+import io.agora.api.example.utils.PermissonUtils;
 
 /**
  * The type Entry fragment.
@@ -85,19 +83,15 @@ public class EntryFragment extends BaseFragment implements View.OnClickListener 
     @Override
     public void onClick(View v) {
         // Check permission
-        if (AndPermission.hasPermissions(this, Permission.Group.STORAGE, Permission.Group.MICROPHONE, Permission.Group.CAMERA)) {
-            join(v);
-            return;
-        }
-        // Request permission
-        AndPermission.with(this).runtime().permission(
-                Permission.Group.STORAGE,
-                Permission.Group.MICROPHONE,
-                Permission.Group.CAMERA
-        ).onGranted(permissions -> {
-            // Permissions Granted
-            join(v);
-        }).start();
+        checkOrRequestPermisson(new PermissonUtils.PermissionResultCallback() {
+            @Override
+            public void onPermissionsResult(boolean allPermissionsGranted, String[] permissions, int[] grantResults) {
+                // Permissions Granted
+                if (allPermissionsGranted) {
+                    join(v);
+                }
+            }
+        });
     }
 
     private void join(View v) {
