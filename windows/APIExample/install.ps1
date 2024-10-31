f8a724bce1a5465dc2fea65dcf30e5ef7e382249
weileifrank <1138289316@qq.com>
2024-10-31 15:59:59 +0800

    feat: modify ci for windows

--- windows/APIExample/install.ps1
+++ windows/APIExample/install.ps1
@@ -1,6 +1,6 @@
+$agora_sdk = 'https://download.agora.io/sdk/release/Agora_Native_SDK_for_Windows_v4.5.0_FULL.zip'
 $ThirdPartysrc = 'https://fullapp.oss-cn-beijing.aliyuncs.com/API-Examples/ThirdParty.zip'
 $ThirdPartydes = 'ThirdParty.zip'
-$agora_sdk = 'https://download.agora.io/sdk/release/Agora_Native_SDK_for_Windows_v4.5.0_FULL.zip'
 $agora_des = 'AgoraSdk.zip'
 $agora_local_sdk = '../../sdk'
 
