6455989f10a10233259af79773cb1d8df5884258
qinhui <>
2025-01-14 15:46:41 +0800

    Adjust the bitrate parameter.

--- iOS/APIExample/APIExample/Examples/Advanced/ThirdBeautify/ByteBeautify/Manager/Config.h
+++ iOS/APIExample/APIExample/Examples/Advanced/ThirdBeautify/ByteBeautify/Manager/Config.h
@@ -5,7 +5,7 @@
 #ifndef Config_h
 #define Config_h
 
-#define LICENSE_NAME ((const char *)"Agora_test_20241014_20241214_io.agora.entfull_4.5.0_2060.licbag")
+#define LICENSE_NAME ((const char *)"Agora_test_io.agora.entfull.licbag")
 
 #define DEBUG_LOG false
 #define TIME_LOG false
