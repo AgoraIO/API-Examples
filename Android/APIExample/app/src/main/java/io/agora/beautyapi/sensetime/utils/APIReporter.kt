c29165a6d80d17dc36e88244b9c4a3f56427e6a7
zhangwei <zw0872@163.com>
2025-03-11 15:02:10 +0800

    [Android] Fxs The worker thread calls writeLog and sendCustomReportMessage.

--- Android/APIExample/app/src/main/java/io/agora/beautyapi/sensetime/utils/APIReporter.kt
+++ Android/APIExample/app/src/main/java/io/agora/beautyapi/sensetime/utils/APIReporter.kt
@@ -1,19 +1,19 @@
 package io.agora.beautyapi.sensetime.utils
 
-import android.util.Log
-import io.agora.rtc2.Constants
 import io.agora.rtc2.RtcEngine
 import org.json.JSONObject
+import java.util.concurrent.Executors
+import java.lang.ref.WeakReference
 
 enum class APIType(val value: Int) {
-    KTV(1),             // K歌
-    CALL(2),            // 呼叫连麦
-    BEAUTY(3),          // 美颜
-    VIDEO_LOADER(4),    // 秒开秒切
-    PK(5),              // 团战
-    VIRTUAL_SPACE(6),   //
-    SCREEN_SPACE(7),    // 屏幕共享
-    AUDIO_SCENARIO(8)   // 音频
+    KTV(1),             // Karaoke
+    CALL(2),            // Call/Co-hosting
+    BEAUTY(3),          // Beauty
+    VIDEO_LOADER(4),    // Instant Loading
+    PK(5),              // Team Battle
+    VIRTUAL_SPACE(6),   // Virtual Space
+    SCREEN_SPACE(7),    // Screen Sharing
+    AUDIO_SCENARIO(8)   // Audio
 }
 
 enum class ApiEventType(val value: Int) {
@@ -31,42 +31,49 @@ object ApiEventKey {
 }
 
 object ApiCostEvent {
-    const val CHANNEL_USAGE = "channelUsage"                 //频道使用耗时
-    const val FIRST_FRAME_ACTUAL = "firstFrameActual"        //首帧实际耗时
-    const val FIRST_FRAME_PERCEIVED = "firstFramePerceived"  //首帧感官耗时
+    const val CHANNEL_USAGE = "channelUsage"                 // Channel usage duration
+    const val FIRST_FRAME_ACTUAL = "firstFrameActual"        // Actual first frame duration
+    const val FIRST_FRAME_PERCEIVED = "firstFramePerceived"  // Perceived first frame duration
 }
 
 class APIReporter(
     private val type: APIType,
     private val version: String,
-    private val rtcEngine: RtcEngine
+    rtcEngine: RtcEngine
 ) {
     private val tag = "APIReporter"
     private val messageId = "agora:scenarioAPI"
     private val durationEventStartMap = HashMap<String, Long>()
     private val category = "${type.value}_Android_$version"
+    private val executorService = Executors.newSingleThreadExecutor()
+    private val rtcEngineRef = WeakReference(rtcEngine)
 
     init {
         configParameters()
     }
 
-    // 上报普通场景化API
+    // Report regular scenario API
     fun reportFuncEvent(name: String, value: Map<String, Any>, ext: Map<String, Any>) {
-        Log.d(tag, "reportFuncEvent: $name value: $value ext: $ext")
-        val eventMap = mapOf(ApiEventKey.TYPE to ApiEventType.API.value, ApiEventKey.DESC to name)
-        val labelMap = mapOf(ApiEventKey.API_VALUE to value, ApiEventKey.TIMESTAMP to getCurrentTs(), ApiEventKey.EXT to ext)
-        val event = convertToJSONString(eventMap) ?: ""
-        val label = convertToJSONString(labelMap) ?: ""
-        rtcEngine.sendCustomReportMessage(messageId, category, event, label, 0)
+        executorService.submit {
+            rtcEngineRef.get()?.let {
+                val eventMap = mapOf(ApiEventKey.TYPE to ApiEventType.API.value, ApiEventKey.DESC to name)
+                val labelMap = mapOf(
+                    ApiEventKey.API_VALUE to value,
+                    ApiEventKey.TIMESTAMP to getCurrentTs(),
+                    ApiEventKey.EXT to ext
+                )
+                val event = convertToJSONString(eventMap) ?: ""
+                val label = convertToJSONString(labelMap) ?: ""
+                it.sendCustomReportMessage(messageId, category, event, label, 0)
+            }
+        }
     }
 
     fun startDurationEvent(name: String) {
-        Log.d(tag, "startDurationEvent: $name")
         durationEventStartMap[name] = getCurrentTs()
     }
 
     fun endDurationEvent(name: String, ext: Map<String, Any>) {
-        Log.d(tag, "endDurationEvent: $name")
         val beginTs = durationEventStartMap[name] ?: return
         durationEventStartMap.remove(name)
         val ts = getCurrentTs()
@@ -75,7 +82,7 @@ class APIReporter(
         innerReportCostEvent(ts, name, cost, ext)
     }
 
-    // 上报耗时打点信息
+    // Report time-consuming event point information
     fun reportCostEvent(name: String, cost: Int, ext: Map<String, Any>) {
         durationEventStartMap.remove(name)
         innerReportCostEvent(
@@ -86,18 +93,21 @@ class APIReporter(
         )
     }
 
-    // 上报自定义信息
+    // Report custom information
     fun reportCustomEvent(name: String, ext: Map<String, Any>) {
-        Log.d(tag, "reportCustomEvent: $name ext: $ext")
-        val eventMap = mapOf(ApiEventKey.TYPE to ApiEventType.CUSTOM.value, ApiEventKey.DESC to name)
-        val labelMap = mapOf(ApiEventKey.TIMESTAMP to getCurrentTs(), ApiEventKey.EXT to ext)
-        val event = convertToJSONString(eventMap) ?: ""
-        val label = convertToJSONString(labelMap) ?: ""
-        rtcEngine.sendCustomReportMessage(messageId, category, event, label, 0)
+        executorService.submit {
+            rtcEngineRef.get()?.let {
+                val eventMap = mapOf(ApiEventKey.TYPE to ApiEventType.CUSTOM.value, ApiEventKey.DESC to name)
+                val labelMap = mapOf(ApiEventKey.TIMESTAMP to getCurrentTs(), ApiEventKey.EXT to ext)
+                val event = convertToJSONString(eventMap) ?: ""
+                val label = convertToJSONString(labelMap) ?: ""
+                it.sendCustomReportMessage(messageId, category, event, label, 0)
+            }
+        }
     }
 
-    fun writeLog(content: String, level: Int) {
-        rtcEngine.writeLog(level, content)
+    private fun writeLog(content: String, level: Int) {
+        rtcEngineRef.get()?.writeLog(level, content)
     }
 
     fun cleanCache() {
@@ -107,11 +117,15 @@ class APIReporter(
     // ---------------------- private ----------------------
 
     private fun configParameters() {
-        //rtcEngine.setParameters("{\"rtc.qos_for_test_purpose\": true}") //测试环境使用
-        // 数据上报
-        rtcEngine.setParameters("{\"rtc.direct_send_custom_event\": true}")
-        // 日志写入
-        rtcEngine.setParameters("{\"rtc.log_external_input\": true}")
+        executorService.submit {
+            rtcEngineRef.get()?.let {
+                // it.setParameters("{\"rtc.qos_for_test_purpose\": true}") // Used for test environment
+                // Data reporting
+                it.setParameters("{\"rtc.direct_send_custom_event\": true}")
+                // Log writing
+                it.setParameters("{\"rtc.log_external_input\": true}")
+            }
+        }
     }
 
     private fun getCurrentTs(): Long {
@@ -119,20 +133,23 @@ class APIReporter(
     }
 
     private fun innerReportCostEvent(ts: Long, name: String, cost: Int, ext: Map<String, Any>) {
-        Log.d(tag, "reportCostEvent: $name cost: $cost ms ext: $ext")
-        writeLog("reportCostEvent: $name cost: $cost ms", Constants.LOG_LEVEL_INFO)
-        val eventMap = mapOf(ApiEventKey.TYPE to ApiEventType.COST.value, ApiEventKey.DESC to name)
-        val labelMap = mapOf(ApiEventKey.TIMESTAMP to ts, ApiEventKey.EXT to ext)
-        val event = convertToJSONString(eventMap) ?: ""
-        val label = convertToJSONString(labelMap) ?: ""
-        rtcEngine.sendCustomReportMessage(messageId, category, event, label, cost)
+        executorService.submit {
+            rtcEngineRef.get()?.let {
+//                writeLog("reportCostEvent: $name cost: $cost ms", Constants.LOG_LEVEL_INFO)
+                val eventMap = mapOf(ApiEventKey.TYPE to ApiEventType.COST.value, ApiEventKey.DESC to name)
+                val labelMap = mapOf(ApiEventKey.TIMESTAMP to ts, ApiEventKey.EXT to ext)
+                val event = convertToJSONString(eventMap) ?: ""
+                val label = convertToJSONString(labelMap) ?: ""
+                it.sendCustomReportMessage(messageId, category, event, label, cost)
+            }
+        }
     }
 
     private fun convertToJSONString(dictionary: Map<String, Any>): String? {
         return try {
             JSONObject(dictionary).toString()
         } catch (e: Exception) {
-            writeLog("[$tag]convert to json fail: $e dictionary: $dictionary", Constants.LOG_LEVEL_WARNING)
+            LogUtils.e(tag, "convert to json fail: $e dictionary: $dictionary")
             null
         }
     }
