635f464457f56e9954ae254f63a98e3ecbe8e266
Smaug <515092329@qq.com>
2025-04-29 10:46:16 +0800

    Update KeyCenter.swift
    Delete Chinese instructions

--- iOS/APIExample-SwiftUI/APIExample-SwiftUI/Common/KeyCenter.swift
+++ iOS/APIExample-SwiftUI/APIExample-SwiftUI/Common/KeyCenter.swift
@@ -19,10 +19,6 @@ class KeyCenter: NSObject {
      If applications need to communicate with each other, they should use the same App ID.
      In order to get the APP ID, you can open the agora console (https://console.agora.io/) to create a project,
      then the APP ID can be found in the project detail page.
-     声网APP ID
-     Agora 给应用程序开发人员分配 App ID，以识别项目和组织。如果组织中有多个完全分开的应用程序，例如由不同的团队构建，
-     则应使用不同的 App ID。如果应用程序需要相互通信，则应使用同一个App ID。
-     进入声网控制台(https://console.agora.io/)，创建一个项目，进入项目配置页，即可看到APP ID。
      */
     @objc
     static let AppId: String = <#APPID#>
@@ -34,10 +30,6 @@ class KeyCenter: NSObject {
      In order to get the APP ID, you can open the agora console (https://console.agora.io/) to create a project with the App Certificate enabled,
      then the APP Certificate can be found in the project detail page.
      PS: If the project does not have certificates enabled, leave this field blank.
-     声网APP证书
-     Agora 提供 App certificate 用以生成 Token。您可以在您的服务器部署并生成 Token，或者使用控制台生成临时的 Token。
-     进入声网控制台(https://console.agora.io/)，创建一个带证书鉴权的项目，进入项目配置页，即可看到APP证书。
-     注意：如果项目没有开启证书鉴权，这个字段留空。
      */
     static let Certificate: String? = nil
     
