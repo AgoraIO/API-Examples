b9b98a29892c091bfad18c19e1486c3650877630
HeZhengQing <hezhengqing@agora.io>
2025-08-04 20:55:26 +0800

    [FIX] add ci log

--- .github/ci/build/modify_ios_keycenter.py
+++ .github/ci/build/modify_ios_keycenter.py
@@ -1,37 +1,50 @@
 import os, sys
 
 def modfiy(path, isReset):
+    print(f'[modify_ios_keycenter] path === {path}, isReset === {isReset}')
     appId = os.environ.get('APP_ID')
+    print(f'[modify_ios_keycenter] appId === {appId}')
     faceCaptureLicense = os.environ.get('FACE_CAPTURE_LICENSE')
+    print(f'[modify_ios_keycenter] faceCaptureLicense === {faceCaptureLicense}')
     with open(path, 'r', encoding='utf-8') as file:
         contents = []
         for num, line in enumerate(file):
             line = line.strip()
             if "static let AppId" in line:
+                print(f'[modify_ios_keycenter] Find `static let AppId` in line {num + 1}')
                 if isReset:
                     line = "static let AppId: String = <#YOUR APPID#>"
                 else:
                     line = f'static let AppId: String = "{appId}"'
+                print(f'[modify_ios_keycenter] line {num + 1} changed to: {line}')
             elif "static let Certificate" in line:
+                print(f'[modify_ios_keycenter] Find `static let Certificate` in line {num + 1}')
                 if isReset:
                     line = "static let Certificate: String? = <#YOUR Certificate#>"
                 else:
                     line = 'static let Certificate: String? = nil'
+                print(f'[modify_ios_keycenter] line {num + 1} changed to: {line}')
             elif "static let FaceCaptureLicense" in line:
+                print(f'[modify_ios_keycenter] Find `static let FaceCaptureLicense` in line {num + 1}')
                 if isReset:
                     line = "static let FaceCaptureLicense: String? = nil"
                 else:
                     line = f'static let FaceCaptureLicense: String? = "{faceCaptureLicense}"'
+                print(f'[modify_ios_keycenter] line {num + 1} changed to: {line}')
             elif "static NSString * const APPID" in line:
+                print(f'[modify_ios_keycenter] Find `static NSString * const APPID` in line {num + 1}')
                 if isReset:
                     line = "static NSString * const APPID = <#YOUR APPID#>"
                 else:
                     line = f'static NSString * const APPID = @"{appId}";'
+                print(f'[modify_ios_keycenter] line {num + 1} changed to: {line}')
             elif "static NSString * const Certificate" in line:
+                print(f'[modify_ios_keycenter] Find `static NSString * const Certificate` in line {num + 1}')
                 if isReset:
                     line = "static NSString * const Certificate = <#YOUR Certificate#>"
                 else:
                     line = 'static NSString * const Certificate = nil;'
+                print(f'[modify_ios_keycenter] line {num + 1} changed to: {line}')
             contents.append(line)
         file.close()
         
