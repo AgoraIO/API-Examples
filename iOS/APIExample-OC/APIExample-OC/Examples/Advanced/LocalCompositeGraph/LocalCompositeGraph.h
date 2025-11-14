c76d269c893baf30e614a8cdfb16d78f7054b529
qinhui <>
2025-11-14 12:04:03 +0800

    Update screen share feature for OC

---
+++ iOS/APIExample-OC/APIExample-OC/Examples/Advanced/LocalCompositeGraph/LocalCompositeGraph.h
@@ -0,0 +1,20 @@
+//
+//  LocalCompositeGraph.h
+//  APIExample-OC
+//
+//  Created by qinhui on 2025/11/14.
+//
+
+#import "BaseViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LocalCompositeGraphEntry : BaseViewController
+
+@end
+
+@interface LocalCompositeGraph : BaseViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
