b1c2532a5fe6a7d65adf5b6033af877653861ec3
zhangwei <zhangwei03@shengwang.cn>
2026-09-07 11:42:19 +0800

    fix: address Android 4.7.0 build blockers

--- Android/APIExample/app/src/main/java/io/agora/beautyapi/faceunity/FaceUnityBeautyAPIImpl.kt
+++ Android/APIExample/app/src/main/java/io/agora/beautyapi/faceunity/FaceUnityBeautyAPIImpl.kt
@@ -42,7 +42,7 @@ import io.agora.base.VideoFrame
 import io.agora.base.VideoFrame.I420Buffer
 import io.agora.base.VideoFrame.SourceType
 import io.agora.base.VideoFrame.TextureBuffer
-import io.agora.base.internal.video.EglBase
+import io.agora.base.internal.video.EglBaseLock
 import io.agora.base.internal.video.TextureBufferPool
 import io.agora.base.internal.video.YuvHelper
 import io.agora.beautyapi.faceunity.utils.APIReporter
@@ -616,7 +616,7 @@ class FaceUnityBeautyAPIImpl : FaceUnityBeautyAPI, IVideoFrameObserver {
                 }
                 val ret = texBufferHelper.invoke {
                     var result = -1
-                    synchronized(EglBase.lock) {
+                    EglBaseLock.lock {
                         result = fuRenderKit.renderWithInput(input).texture?.texId ?: -1
                     }
                     result
@@ -730,7 +730,7 @@ class FaceUnityBeautyAPIImpl : FaceUnityBeautyAPI, IVideoFrameObserver {
                 return@invoke -1
             }
             var fuTexId = -1
-            synchronized(EglBase.lock) {
+            EglBaseLock.lock {
                 fuTexId = fuRenderKit.renderWithInput(input).texture?.texId ?: -1
             }
             outGLFrameBuffer.setSize(videoFrame.rotatedWidth, videoFrame.rotatedHeight)
@@ -853,7 +853,7 @@ class FaceUnityBeautyAPIImpl : FaceUnityBeautyAPI, IVideoFrameObserver {
             }
 
             var fuTexId = -1
-            synchronized(EglBase.lock) {
+            EglBaseLock.lock {
                 fuTexId = fuRenderKit.renderWithInput(input).texture?.texId ?: -1
             }
             outGLFrameBuffer.setSize(videoFrame.rotatedWidth, videoFrame.rotatedHeight)
